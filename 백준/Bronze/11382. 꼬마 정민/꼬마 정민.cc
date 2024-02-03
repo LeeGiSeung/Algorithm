8915d832f58f1dd57cc9c6d3f0e745244d1c8a87
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-03 14:49:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	long long A, B, C;
+
+	cin >> A >> B >> C;
+	cout << A + B + C << endl;
+
+	return 0;
+}
