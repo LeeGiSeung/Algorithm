ca4d0404967fb7e2d9b493b6aececa2548467acb
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-03 16:41:10 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+
+	int n;
+	string s;
+
+	cin >> n;
+
+	while (n--) {
+		cin >> s;
+		cout << s[0] << s[s.length() - 1] << '\n';
+	}
+
+	return 0;
+}
