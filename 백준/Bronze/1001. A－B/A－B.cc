4126ae34bf93ad60ad036377283902164e8ed15a
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-03 14:40:31 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << A - B;
+	return 0;
+
+}
