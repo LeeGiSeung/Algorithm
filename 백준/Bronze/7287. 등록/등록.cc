3f7444a48e83b7916bfae855b5e28e4a4d63151f
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-03 14:35:30 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+	cout << 114 << endl;
+	cout << "kimbab1004" << endl;
+	return 0;
+}
