1d40c536e2bfeaa13aaa7f6e6b3ae8bdeb19889e
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-03 16:37:19 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main() {
+
+	string str;
+	while (1) {
+		getline(cin, str);
+		if (str == ""){
+			break;
+		}
+		else
+			cout << str << "\n";
+	}
+
+}
