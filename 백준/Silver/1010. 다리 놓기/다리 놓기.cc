ab8bd3db8c1ea4b91afd96ee9f3cce267b116a1e
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-04 13:47:54 +0900

    [Silver V] Title: 다리 놓기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcase, n, r, temp;
+    int answer;
+
+    cin >> tcase;
+    while (tcase--) {
+        answer = 1;
+        temp = 1;
+        cin >> r >> n;
+        for (int i = n; i > n - r; i--) {
+            answer *= i;
+            answer /= temp++;
+        }
+        cout << answer << endl;
+    }
+}
