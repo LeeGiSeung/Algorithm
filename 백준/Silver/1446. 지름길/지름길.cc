39198fc4f4b7c58245eec20b5d2894f4cf90e2d3
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-08 13:17:02 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2132 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.cc
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -1,34 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
 
-#define MAX 10001
-#define INF 987654321
+#define MAX 987654321
 
 using namespace std;
-using pii=pair<int, int>;
 
-vector<pii> vec[MAX];
-vector<int> dist(MAX, INF);
 
-int main(){
-    int N, D; cin>>N>>D;
-    
-    while(N--){
-        int s,e,w; cin>>s>>e>>w;
-        if(e>D)continue;
-        vec[e].push_back({s, w});
+int main(void) {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int n, d, from, to, dis;
+    cin >> n >> d;
+    vector<int> map(d + 1, MAX);
+    vector<pair<int, int>> sc[10001];
+    for (int i = 0; i < n; i++) {
+        cin >> from >> to >> dis;
+        sc[to].push_back({ from,dis });
     }
-    
-    dist[0]=0;
-    
-    for(int i=1; i<=D; i++){
-        if(vec[i].size()==0) dist[i]=dist[i-1]+1;
-        else{
-            for(auto d: vec[i])
-                dist[i]=min(dist[i], min(dist[i-1]+1, dist[d.first]+d.second));
+    map[0] = 0;
+    for (int i = 1; i <= d; i++) {
+        if (sc[i].size() == 0) map[i] = map[i - 1] + 1;
+        else {
+            for (auto v : sc[i]) { 
+                map[i] = min(map[i], min(map[i - 1] + 1, map[v.first] + v.second));
+            }
         }
     }
-
-    cout<<dist[D];
-    
+    cout << map[d] << "\n";
     return 0;
 }
