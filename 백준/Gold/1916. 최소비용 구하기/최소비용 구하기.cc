482a8e9ed4cd6b105b4cfa0de1a5e7d6c852a054
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-02-19 19:17:19 +0900

    [Gold V] Title: 최소비용 구하기, Time: 36 ms, Memory: 4744 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,63 +1,62 @@
 #include <iostream>
-#include <queue>
+#include <deque>
+#include <string>
+#include <sstream>
 #include <vector>
+#include <string>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 int n, m;
 int dp[1001];
-vector<pair <int, int>> road[1001];
+vector<pair<int, int>> ary[1001];
 
 void bfs(int start) {
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0,start });
 
-    priority_queue<pair<int, int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    pq.push({ 0, start });
+	while (!pq.empty()) {
+		int current = pq.top().second;
+		int cost = pq.top().first;
+		pq.pop();
+		if (cost > dp[current]) {
+			continue;
+		}
+		for (int i = 0; i < ary[current].size(); i++) {
+			int next = ary[current][i].first;
+			int next_cost = dp[current] + ary[current][i].second;
 
-    while (!pq.empty()) {
-        int current = pq.top().second;
-        int cost = pq.top().first;
-        pq.pop();
-        if (cost > dp[current]) {
-            continue;
-        }
-
-        for (int i = 0; i < road[current].size(); i++) {
-            int next = road[current][i].first;
-            int next_cost = dp[current] + road[current][i].second;
-
-            if (next_cost < dp[next]) {
-                dp[next] = next_cost;
-                pq.push({ next_cost,next });
-            }
-
-        }
-
-
-    }
-
+			if (next_cost < dp[next]) {
+				dp[next] = next_cost;
+				pq.push({ next_cost,next });
+			}
+		}
+	}
+	return;
 }
 
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	cin >> n;
+	cin >> m;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		ary[x].push_back({ y,z });
+	}
+	int go, goal;
+	cin >> go >> goal;
+	fill_n(dp, 1001, 987654321);
+	dp[go] = 0;
+	bfs(go);
 
-    for (int i = 0; i < m; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        road[a].push_back({ b,c });
-        //a 시작 b도착 c금액
-    }
-    int start, end;
-    cin >> start >> end;
-    fill_n(dp, 1001, 987654321);
-    dp[start] = 0;
-    bfs(start);
+	cout << dp[goal];
 
-    cout << dp[end];
+	return 0;
 
-    return 0;
 }
