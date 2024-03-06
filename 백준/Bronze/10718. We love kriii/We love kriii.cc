fa02265490f13f0fe6bc7e22497b4b6ebd117b59
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-06 18:41:07 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>  // #include <cstdio> 대치 가능
+ 
+int main(int argc, char const *argv[]) {
+ 
+    printf("강한친구 대한육군\n강한친구 대한육군");
+    /*
+    또는 
+    printf("강한친구 대한육군\n");
+    printf("강한친구 대한육군");
+    */
+    return 0;
+}
