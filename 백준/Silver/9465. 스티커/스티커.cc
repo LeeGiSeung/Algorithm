43e8eb71e4724c4c0a80736e33453280a8277ed2
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-07 13:15:28 +0900

    [Silver I] Title: 스티커, Time: 80 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,34 +1,56 @@
 #include <iostream>
+#include <deque>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <queue>
 #include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+#include <cstring>
+
 using namespace std;
-long long arr[2][100001]; //Array Data
-long long dp[2][100001];  //Max
+int ary[2][100001] = { 0 };
+int dp[2][100001] = { 0 };
+int T;
+int n;
 
-int main() { 
-	int testCase;
-	int n;
-	cin >> testCase;
 
-	for (int t = 0; t < testCase; t++) {
+void input() {
 		cin >> n;
-		for (int i = 0; i < 2; i++) {
-			for (int j = 1; j < n+1; j++) {
-				cin >> arr[i][j];
+		for (int z = 0; z < 2; z++)
+		{
+			for (int j = 1; j <= n; j++) {
+				cin >> ary[z][j];
 			}
 		}
+}
 
-		//init
-		dp[0][0] = 0;
-		dp[1][0] = 0;
-		dp[0][1] = arr[0][1];
-		dp[1][1] = arr[1][1];
+void dp_cal() {
 
-		for (int j = 2;j < n + 1;j++) {
-			dp[0][j] = max(dp[1][j - 2], dp[1][j - 1]) + arr[0][j];
-			dp[1][j] = max(dp[0][j - 2], dp[0][j - 1]) + arr[1][j];
+		dp[0][0] = dp[1][0] = 0;
+		dp[0][1] = ary[0][1];
+		dp[1][1] = ary[1][1];
+
+		for (int j = 2; j <= n; j++) {
+			dp[0][j] = max(dp[1][j - 1], dp[1][j - 2])+ary[0][j];
+			dp[1][j] = max(dp[0][j - 1], dp[0][j - 2])+ary[1][j];
 		}
+		cout << max(dp[0][n], dp[1][n]) << "\n";
+}
 
-		cout << max(dp[0][n], dp[1][n]) << '\n';
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	for (int k = 0; k < T; k++) {
+
+		input();
+		dp_cal();
 	}
 
 	return 0;
