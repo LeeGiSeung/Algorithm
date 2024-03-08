1c3c2b5ca13afaf60b099cdbb331c2a3b5670639
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-08 14:49:38 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int c, n, cnt;
+	int score[1000];
+	double avg;
+
+	scanf("%d", &c);
+
+	for (int i = 0; i < c; ++i) {
+		scanf("%d", &n);
+		avg = 0.0;
+
+		for (int j = 0; j < n; ++j) {
+			scanf("%d", &score[j]);
+			avg += score[j];
+		}
+		avg /= n;
+
+		cnt = 0;
+		for (int j = 0; j < n; ++j) {
+			if (score[j] > avg) ++cnt;
+		}
+
+		printf("%.3f%\n", 100.0 * cnt / n);
+	}
+
+	return 0;
+}
