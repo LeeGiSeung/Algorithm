fbca05f67aa96edd5d8ac625a3bff91e28e796a4
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-17 22:20:30 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+	int A, B, C;
+	cin >> A >> B >> C;
+ 
+	int min = 60 * A + B;   // 시 -> 분
+	min += C;
+ 
+	int hour = (min / 60) % 24;
+	int minute = min % 60;
+ 
+	cout << hour << " " << minute;
+	return 0;
+}
