5edfc3e160efbad3cef2a92eed414d75c2cc415c
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-18 18:07:37 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+ 
+	int N;
+	cin >> N;
+ 
+	for (int i = N; i > 0; i--) {
+		cout << i << "\n";
+	}
+	return 0;
+}
