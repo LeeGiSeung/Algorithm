8f4dc13d28216a746d2f897230f1e3b60f450ce0
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-03-29 00:46:47 +0900

    Update 문자열 폭발.cc

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,34 +1,29 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <deque>
+#include <string>
+
 using namespace std;
-int main(){
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    string a; // 전체 문자열 변수
-    string b; // 폭발 문자열 변수
-    string t = ""; // 임시 문자열 
-    cin >> a >> b;
-    int a_len = a.length(); // 전체 문자열 길이
-    int b_len = b.length(); // 폭발 문자열 길이
-    for (int i = 0; i < a.length(); i++)
-    {
-        t += a[i]; // 문자 추가
-        if(t.length()>=b_len){ // 임시 문자 길이가 폭발 문자열 보다 크거나 같을 때
-            bool flag = true; // 폭발 문자열 있는지 확인하는 flag
-            for (int j = 0; j < b_len; j++){ 
-                if(t[t.length()-b_len+j] != b[j]){
-                    flag = false;
-                    break;
-                } // t뒤에서 폭발 문자열 길이만큼 비교
-            }
- 
-            if(flag) // 폭발 문자열일 경우 삭제 
-                t.erase(t.end() - b_len, t.end());
+    cout.tie(NULL);
+
+    string str, bomb, ret;
+
+    cin >> str >> bomb;
+
+    for (char c : str) {
+        ret += c;
+        if (ret.size() >= bomb.size() &&
+            ret.substr(ret.size() - bomb.size(), bomb.size()) == bomb) {
+            ret.erase(ret.end() - bomb.size(), ret.end());
         }
     }
-        if (t.empty()) // 남아 있는 문자열이 없는 경우
-            cout << "FRULA" << '\n';
-        else
-            cout << t << '\n';
+
+    if (ret.size()) cout << ret << "\n";
+    else cout << "FRULA\n";
+
     return 0;
+
 }
