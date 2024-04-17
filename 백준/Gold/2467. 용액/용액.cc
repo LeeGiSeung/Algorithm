3974be615102e7b619deae3b58cac39792dfe536
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-04-17 18:23:10 +0900

    Update 용액.cc

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -3,30 +3,37 @@
 
 using namespace std;
 int n;
-int arr[100001];
+int solution[100001];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
+        cin >> solution[i];
     }
 
-    int ans = 2000000001;
+    int result = 2000000001;
 
-    int start = 1, end = n;
-    int first, second;
-    while (start < end) {
-        int now = arr[start] + arr[end];
+    int startP = 1;
+    int endP = n;
+    int startRe, endRe;
+
+    while (startP < endP) {
+        int now = solution[startP] + solution[endP];
         int num = abs(now);
-        if (ans > num) {
-            ans = num;
-            first = arr[start], second = arr[end];
+        if (result >= num) {
+            result = num;
+            startRe = solution[startP];
+            endRe = solution[endP];
         }
-        if (now > 0) end--;
-        else start++;
+        if (now > 0) endP--;
+        else startP++;
     }
-    cout << first << " " << second;
+
+    cout << startRe << " " << endRe << "\n";
+
+
     return 0;
 }
