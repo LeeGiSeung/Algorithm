170faa920f31dbe163c0dae528b05a9ac3b12743
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-04-17 17:16:11 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -9,7 +9,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cin >> n;
-    for(int i=1 ; i<=n ; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
 
@@ -17,14 +17,14 @@ int main() {
 
     int start = 1, end = n;
     int first, second;
-    while(start<end) {
+    while (start < end) {
         int now = arr[start] + arr[end];
         int num = abs(now);
-        if(ans>num) {
+        if (ans > num) {
             ans = num;
             first = arr[start], second = arr[end];
         }
-        if(now>0) end--;
+        if (now > 0) end--;
         else start++;
     }
     cout << first << " " << second;
