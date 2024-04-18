4cc834b6f2e62dc6be91ebf4ae6aea2da58afdf2
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-04-18 12:18:46 +0900

    Update 수 나누기 게임.cc

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,52 +1,47 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
+int n;
+int v[1000002];
+int card[10000002];
+int score[1000002] = { 0 };
 
-vector<int> numbers;
-int scores[1000006];
-int cards[1000006];
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        card[i] = a;
+        v[a] = 1;
+    }
+}
 
-	// 플레이어 수 입력
-	int N;
-	cin >> N;
+void solve() {
+    for (int i = 0; i < n; i++) {
+        for (int j = card[i]; j < 1000002; j += card[i]) {
+            if (v[j] == 1) {
+                score[card[i]]++;
+                score[j]--;
+            }
+        }
+    }
 
-	// 각 플레이어의 카드 넘버 입력
-	for (int i = 0; i < N; i++)
-	{
-		int temp;
-		cin >> temp;
+    for (int i = 0; i < n; i++) {
+        cout << score[card[i]] << " ";
+    }
 
-		numbers.push_back(temp); // 각 플레이어의 카드 넘버 저장
-		cards[temp] = 1; // 특정 플레이어가 해당 카드 넘버를 가지고 있는지 체크
-	}
+}
 
-	// 에라토스테네스의 체 응용
-	// 각 플레이어의 카드 넘버의 배수를 각각 체크하면서 해당 카드가 있다면, 결투
-	for (int i = 0; i < N; i++)
-	{
-		// 해당 카드 넘버의 배수마다 탐색
-		for (int j = numbers[i] * 2; j < 1000001; j += numbers[i])
-		{
-			// 해당 카드 넘버의 배수의 카드가 존재하면 결투
-			if (cards[j] == 1)
-			{
-				scores[numbers[i]]++;
-				scores[j]--;
-			}
-		}
-	}
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-	// 각 플레이어의 점수 출력
-	for (int i = 0; i < N; i++)
-		cout << scores[numbers[i]] << " ";
+    input();
+    solve();
 
-	return 0;
+    return 0;
+
+
 }
