7c9721e654fb340113cbfb2700a397305b310067
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-05-06 23:04:51 +0900

    Update RGB거리 2.cc

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,37 +1,47 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#define MAX 1000*1000+1
+#include <queue>
+
 using namespace std;
-int cost[1001][3];
-int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n;
-    int ans = MAX;
-    cin>>n;
-    int r,g,b;
-    for(int i=1;i<=n;i++){
-        cin>>r>>g>>b;
-        cost[i][0]=r;
-        cost[i][1]=g;
-        cost[i][2]=b;
-    }
-    
-    for(int start=0; start<=2; start++){
-        int dp[1001][3];
-        for(int j=0;j<=2;j++){
-            if(j==start) dp[1][start]=cost[1][start];
-            else dp[1][j]= MAX;
-        }
-        for(int i=2;i<=n;i++){
-            dp[i][0] = min(dp[i-1][1],dp[i-1][2])+cost[i][0];
-            dp[i][1] = min(dp[i-1][0],dp[i-1][2])+cost[i][1];
-            dp[i][2] = min(dp[i-1][1],dp[i-1][0])+cost[i][2];
-        }
-        for(int j=0;j<=2;j++){
-            if(j==start) continue;
-            ans = min(ans, dp[n][j]);
-        }
-    }
-    cout<<ans;
-    return 0;
+int n;
+int cnt = 1000001;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	int house[100][100];
+	
+	house[0][0] = 0;
+	house[0][1] = 0;
+	house[0][2] = 0;
+
+	for (int i = 1; i <= n; i++) {
+		int r, g, b;
+		cin >> r >> g >> b;
+		house[i][0] = r;
+		house[i][1] = g;
+		house[i][2] = b;
+	}
+
+	for (int start = 0; start <= 2; start++) {
+		int dp[1001][3];
+		for (int j = 0; j <= 2; j++) {
+			if (start == j) dp[1][start] = house[1][start];
+			else dp[1][j] = 1000001;
+		}
+		for (int i = 2; i <= n; i++) {
+			dp[i][0] = min(dp[i - 1][1] , dp[i - 1][2]) + house[i][0];
+			dp[i][1] = min(dp[i - 1][0] , dp[i - 1][2]) + house[i][1];
+			dp[i][2] = min(dp[i - 1][0] , dp[i - 1][1]) + house[i][2];
+		}
+		for (int j = 0; j <= 2; j++) {
+			if (start == j) continue;
+			cnt = min(dp[n][j], cnt);
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
 }
