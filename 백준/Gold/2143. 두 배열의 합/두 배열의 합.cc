6649c76cc462205687b275227028e7c16eb1f042
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-05-07 16:05:30 +0900

    Update 두 배열의 합.cc

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -1,53 +1,57 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
+int t, n, m;
+long long cnt = 0;
+vector<int> v, w;
 
-int T, N, M;
-int A[1001], B[1001];
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-int main(void){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+    cin >> n;
 
-	cin >> T >> N;
-	for (int i = 0; i < N; i++) 
-        cin >> A[i];
-	cin >> M;
-	for (int i = 0; i < M; i++) 
-        cin >> B[i];
+    vector<int> v1(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v1[i];
+    }
+    cin >> m;
+    vector<int> v2(m);
+    for (int i = 0; i < m; i++) {
+        cin >> v2[i];
+    }
 
-	vector<int> aSum, bSum;
-	
-	for (int i = 0; i < N; i++) {
-		int sum = A[i];
-		aSum.push_back(sum);
-		for (int j = i + 1; j < N; j++) {
-			sum += A[j];
-			aSum.push_back(sum);
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        int sum = v1[i];
+        v.push_back(sum);
+        for (int j = i + 1; j < n; j++) {
+            sum += v1[j];
+            v.push_back(sum);
+        }
+    }
 
-	for (int i = 0; i < M; i++) {
-		int sum = B[i];
-		bSum.push_back(sum);
-		for (int j = i + 1; j < M; j++) {
-			sum += B[j];
-			bSum.push_back(sum);
-		}
-	}
+    for (int i = 0; i < m; i++) {
+        int sum = v2[i];
+        w.push_back(sum);
+        for (int j = i + 1; j < m; j++) {
+            sum += v2[j];
+            w.push_back(sum);
+        }
+    }
 
-	sort(bSum.begin(), bSum.end());
-	long long ans = 0;
-	for (int i = 0; i < aSum.size(); i++) {
-		int target = T - aSum[i];
-        // 타겟과 같은 수들 개수 (hi - lo)
-		int lo = lower_bound(bSum.begin(), bSum.end(), target) - bSum.begin();
-		int hi = upper_bound(bSum.begin(), bSum.end(), target) - bSum.begin();
-		ans += (hi - lo);
-	}
+    sort(v2.begin(), v2.end());
 
-	cout << ans;
+    for (int i = 0; i < v.size(); i++) {
+        int c = t - v[i];
+        int lower = lower_bound(w.begin(), w.end(), c) - w.begin();
+        int upper = upper_bound(w.begin(), w.end(), c) - w.begin();
 
-	return 0;
+        cnt += (upper - lower);
+    }
+
+    cout << cnt;
+    return 0;
+
 }
