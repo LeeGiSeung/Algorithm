66bfd54007d4e4e7e8bcd9f145062d22e5659dab
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-06-24 15:22:50 +0900

    [Silver I] Title: 스타트와 링크, Time: 64 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -10,38 +10,26 @@ deque<int> link;
 int start_result = 0, link_result = 0, result = 100000000;
 bool visit[25];
 
-void DFS(int x, int pos) // x는 카운트 수, pos는 다음 값
-{
-	if (x == n / 2) // 카운트수가 정원의 1/2이 됐을 때 능력치합 계산
-	{
-		int start, link;
-		start = 0;
-		link = 0;
-
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-			{
-				if (visit[i] == true && visit[j] == true) start += soccer[i][j];
-				if (visit[i] == false && visit[j] == false) link += soccer[i][j];
-			}
-		}
-
-		int temp = abs(start - link);
-		if (result > temp) result = temp;
-
-		return;
-	}
-
-	for (int i = pos; i < n; i++)
-	{
-		visit[i] = true;
-		DFS(x + 1, i + 1);
-		visit[i] = false;
-	}
-
+void dfs(int x, int y) {
+    if (x == n / 2) {
+        int start_result = 0, link_result = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (visit[i] == true && visit[j] == true) start_result += soccer[i][j];
+                else if (visit[i] == false && visit[j] == false) link_result += soccer[i][j];
+            }
+        }
+        int temp = abs(start_result - link_result);
+        if (result > temp) result = temp;
+    }
+    else {
+        for (int k = y; k < n; k++) {
+            visit[k] = true;
+            dfs(x + 1, k + 1);
+            visit[k] = false;
+        }
+    }
 }
-
 
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
@@ -53,7 +41,7 @@ int main() {
         }
     }
 
-	DFS(0, 1);
+    dfs(0, 1);
 
     cout << result << "\n";
 
