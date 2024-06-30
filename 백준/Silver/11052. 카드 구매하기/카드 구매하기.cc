150764adc31ee1dca299eafb9677a5b7b6952089
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-06-30 16:04:50 +0900

    Update 카드 구매하기.cc

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
+#include <cstring>
+
+#define endl "\n"
 
 using namespace std;
+int n;
+int pack[1000];
+int result = 0;
+int dp[1001];
 
-const int MAX = 1001;
-int dp[MAX];
-int cards[MAX];
 
-int main(){
-    int n;
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
     cin >> n;
-    
-    for(int i=1;i<=n;i++)
-        cin >> cards[i];
-    
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=i;j++)
-            dp[i] = max(dp[i], dp[i-j] + cards[j]);
-    
+
+    for (int i = 1; i <= n; i++) {
+        cin >> pack[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] = max(dp[i], dp[i - j] + pack[j]);
+        }
+    }
+
     cout << dp[n] << endl;
-    
+
     return 0;
+
 }
