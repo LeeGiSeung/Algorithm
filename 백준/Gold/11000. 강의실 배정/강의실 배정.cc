c057e8d9cfaba2632f978cde4531d7ebb45e082e
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-07-07 17:35:53 +0900

    [Gold V] Title: 강의실 배정, Time: 196 ms, Memory: 5108 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,33 +1,41 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 #include<queue>
-#include<cstring>
+#include<stdio.h>
+#include<algorithm>
+
+#define endl "\n"
+
 using namespace std;
-int N;
-int answer;
+int n;
+int result = 0;
 vector<pair<int, int>> v;
-priority_queue<int, vector<int>, greater<int>> pq;
- 
-int main() {
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        int start, end;
-        cin >> start >> end;
-        v.push_back({ start,end });
-    }
-    //먼저 시작하는 회의 순으로 정렬
-    sort(v.begin(), v.end()); 
- 
-    pq.push(v[0].second);
-    for (int i = 1; i < v.size(); i++) {
-        if (v[i].first >= pq.top()) {
-            pq.pop();
-            pq.push(v[i].second);
-        }
-        else {
-            pq.push(v[i].second);
-        }
-    }
-    cout << pq.size();
+priority_queue<int, vector<int>, greater<int>> q;
+
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		v.push_back({ a,b });
+	}
+
+	sort(v.begin(), v.end());
+
+	q.push(v[0].second);
+
+	for (int i = 1; i < n; i++) {
+		if (v[i].first >= q.top()) {
+			q.pop();
+			q.push(v[i].second);
+		}
+		else q.push(v[i].second);
+	}
+
+	cout << q.size() << endl;
+
+	return 0;
 }
