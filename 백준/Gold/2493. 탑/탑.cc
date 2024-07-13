7a01f3bb4b3bfde0e13ceee1425a08f4ce3bc32b
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-07-13 21:13:21 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,25 +1,39 @@
-#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <queue>
 #include <stack>
-#include <utility>
+
+#define endl "\n"
+
 using namespace std;
-int n, k;
-stack<pair<int, int> > st;
-int main() {
-    scanf("%d", &n);
-    for (int i = 1; i<=n; i++) {
-        scanf("%d", &k);
-        while (!st.empty()) {
-            //스택의 top이 현재 입력값보다 크다면 신호 수신 가능이므로 
-            if (st.top().second > k){
-                //top의 위치를 출력하고 반복문을 탈출한다. 
-                printf("%d ", st.top().first);
+int n,a;
+
+stack<pair<int,int>> s;
+
+void input() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a;
+        while (!s.empty()) {
+            if (a <= s.top().second) {
+                cout << s.top().first<<" ";
                 break;
             }
-            st.pop();
+            else {
+                s.pop();
+            }
         }
-        //스택이 비었으면 0을 출력한다. 
-        if(st.empty()) printf("0 ");
-        //그리고 현재 탑을 스택에 push 
-        st.push(make_pair(i, k));
+        if (s.empty()) {
+            cout << "0 ";
+        }
+        s.push({ i,a });
     }
+
+}
+
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    input();
+    return 0;
 }
