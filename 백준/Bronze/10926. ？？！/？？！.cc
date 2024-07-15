d49986f4b09d786d521941a7646cf1e6229ab985
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-07-15 16:27:53 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define endl "\n"
+
+using namespace std;
+
+string s;
+
+void input() {
+    cin >> s;
+    cout << s << "??!" << endl;
+
+}
+
+int main() {
+
+    input();
+
+    return 0;
+}
