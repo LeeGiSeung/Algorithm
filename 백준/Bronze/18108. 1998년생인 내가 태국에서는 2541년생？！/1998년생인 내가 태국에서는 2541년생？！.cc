5b1fcebc3d2412c01da217884e3ef3dfcfa09d98
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-07-15 16:29:23 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define endl "\n"
+
+using namespace std;
+
+int n;
+
+void input() {
+    cin >> n;
+    cout <<n-543<< endl;
+
+}
+
+int main() {
+
+    input();
+
+    return 0;
+}
