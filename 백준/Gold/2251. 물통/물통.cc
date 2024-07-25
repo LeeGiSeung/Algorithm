867fd3e5c10d2c7a550e2bf45160b3bb0eb3679a
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-07-25 15:37:15 +0900

    [Gold V] Title: 물통, Time: 0 ms, Memory: 9960 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.cc
+++ 백준/Gold/2251. 물통/물통.cc
@@ -1,107 +1,127 @@
 #include <iostream>
-#include <cstdio>
+#include <algorithm>
 #include <queue>
+#include <vector>
+#include <deque>
 #include <set>
 
+
 using namespace std;
+int max_a, max_b, max_c;
 
-queue<pair<pair<int, int>, int>> check;
-int visited[201][201][201];
-set<int> can_c;
+bool visit[201][201][201] = { false };
 
-int main (void){
-    int a, b, c;
-    int p_a, p_b, p_c;
-    int n_a, n_b, n_c;
-    int move;
-    set<int>::iterator it;
-    
-    scanf("%d %d %d", &a, &b, &c);
-    
-    check.push(make_pair(make_pair(0, 0),c));
-    can_c.insert(c);
-    
-    while(!check.empty()){
-        p_a = check.front().first.first;
-        p_b = check.front().first.second;
-        p_c = check.front().second;
-        check.pop();
-        
-        if(visited[p_a][p_b][p_c])
+set<int> result;
+int mv;
+int na, nb, nc;
+void bfs() {
+
+    queue < pair<pair<int, int>, int >> q;
+
+    q.push({ {0,0},max_c });
+
+    result.insert(max_c);
+
+    while (!q.empty()) {
+
+        int ia = q.front().first.first;
+        int ib = q.front().first.second;
+        int ic = q.front().second;
+        q.pop();
+
+        if (visit[ia][ib][ic] == true) {
             continue;
-        
-        visited[p_a][p_b][p_c] = 1;
-        
-       // printf("%d %d %d\n", p_a, p_b, p_c);
-        
-        if(p_a!=0){
-            if(p_b!=b){      //A->B
-                move = min(p_a, b - p_b);
-                n_a = p_a - move;
-                n_b = p_b + move;
-                n_c = p_c;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
+        }
+
+        visit[ia][ib][ic] = true;
+
+        //a에서 움직임
+        if (ia != 0) {
+            if (ib != max_b) {
+                mv = min(ia,max_b - ib);
+                na = ia - mv;
+                nb = ib + mv;
+                nc = ic;
+                q.push({ {na,nb},nc });
+                if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
             }
-            if(p_c!=c){     //A->C
-                move = min(p_a, c - p_c);
-                n_a = p_a - move;
-                n_b = p_b;
-                n_c = p_c + move;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
+            if (ic != max_c) {
+                mv = min(ia,max_c - ic);
+                na = ia - mv;
+                nb = ib;
+                nc = ic + mv;
+                q.push({ {na,nb},nc });
+                if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
+            }
+        }
+
+        //b에서 움직임
+        if (ib != 0) {
+            if (ia != max_a) {
+                mv = min(ib,max_a - ia);
+                na = ia + mv;
+                nb = ib - mv;
+                nc = ic;
+                q.push({ {na,nb},nc });
+                if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
+            }
+            if (ic != max_c) {
+                mv = min(ib,max_c - ic);
+                na = ia;
+                nb = ib - mv;
+                nc = ic + mv;
+                q.push({ {na,nb},nc });
+                if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
+            }
+        }
+
+        //c에서 움직임
+        if (ic != 0) {
+            if (ib != max_b) {
+                mv = min(ic,max_b - ib);
+                na = ia;
+                nb = ib + mv;
+                nc = ic - mv;
+                q.push({ {na,nb},nc });
+                    if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
+            }
+            if (ia != max_a) {
+                mv = min(ic,max_a - ia);
+                na = ia + mv;
+                nb = ib;
+                nc = ic - mv;
+                q.push({ {na,nb},nc });
+                if (na == 0 && visit[na][nb][nc] == false) {
+                    result.insert(nc);
+                }
             }
         }
         
-        if(p_b!=0){
-            if(p_a!=a){      //B->A
-                move = min(p_b, a - p_a);
-                n_a = p_a + move;
-                n_b = p_b - move;
-                n_c = p_c;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
-           
-            }
-            if(p_c!=c){     //B->C
-                move = min(p_b, c - p_c);
-                n_a = p_a;
-                n_b = p_b - move;
-                n_c = p_c + move;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
-            }
-        }
-        
-        if(p_c!=0){
-            if(p_a!=a){      //C->A
-                move = min(p_c, a - p_a);
-                n_a = p_a + move;
-                n_b = p_b;
-                n_c = p_c - move;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
-           
-            }
-            if(p_b!=b){     //C->B
-                move = min(p_c, b - p_b);
-                n_a = p_a;
-                n_b = p_b + move;
-                n_c = p_c - move;
-                check.push(make_pair(make_pair(n_a, n_b),n_c));
-                if(n_a == 0)
-                    can_c.insert(n_c);
-            }
-        }
+
     }
-    
-    for (it = can_c.begin(); it != can_c.end(); it++) {
-        printf("%d ", *it);
+
+    for (int value : result){
+        cout << value << " ";
     }
-    printf("\n");
+
+
+}
+
+int main() {
+
+    cin >> max_a >> max_b >> max_c;
+ 
+    bfs();
+
+    return 0;
 }
