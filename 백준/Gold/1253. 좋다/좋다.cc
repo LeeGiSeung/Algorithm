e983964d3e1470d36483d19ab141bc4be1325379
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-08-05 15:09:27 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <vector>
+
+using namespace std;
+int n;
+vector<int> v;
+
+int good = 0;
+
+//두개의 합
+//1
+//2
+//3 2, 1
+//4 3, 1
+//5 3, 2
+//6 4, 2
+//7 4, 3
+//8 5, 3
+//9 5, 4
+//10 7, 3
+
+//완전 탐색하면 뻑날거고 투포인터?
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	if (n < 3) {
+		cout << 0;
+		return 0;
+	}
+
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < n; i++) {
+
+		int s = 0;
+		int e = n-1;
+
+		while (s < e){
+			if (v[s] + v[e] < v[i]) {
+				s++;
+			}
+			else if (v[s] + v[e] > v[i]) {
+				e--;
+			}
+			else {
+				if (s == i) s++;
+				else if (e == i) e--;
+				else {
+					good++;
+					break;
+				}
+			}
+		}
+	}
+	
+	cout << good;
+
+	return 0;
+}
