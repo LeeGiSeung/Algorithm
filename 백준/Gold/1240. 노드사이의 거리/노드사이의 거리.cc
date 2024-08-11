cd25a92bdec57e0ab8679e3da94cd6f061e503dd
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-08-11 23:27:51 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,68 +1,63 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
 #include <vector>
-#include <algorithm>
-#define FIRST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
-#define MAX 1005
-#define LL long long
-#define INF 2e9
+#include <queue>
+#include <cstring>
 
 using namespace std;
-int N, M;
-vector<pair<int, int> > Vec[MAX];
-bool visited[MAX];
 
-void init() {
-	for (int i = 0; i < MAX; i++) {
-		visited[i] = false;
-	}
-}
+int n,m;
+vector<pair<int, int>> v[1005];
+bool visit[1005] = { false };
 
-int BFS(int X, int Y) {
-	init();
-	queue<pair<int, int> > Q;
-	visited[X] = true;
-	Q.push(make_pair(X, 0));
-
-	while (!Q.empty()) {
-		int CurX = Q.front().first;
-		int CurCost = Q.front().second;
-		Q.pop();
-
-		if (CurX == Y) {
-			return CurCost;
+int solve(int s, int e) {
+	visit[s] = true;
+	queue<pair<int,int>> q;
+	int distance = 0;
+	q.push({ s,0 });
+	while (!q.empty()) {
+		s = q.front().first;
+		distance = q.front().second;
+		q.pop();
+		
+		if (s == e) {
+			return distance;
 		}
 
-		for (int i = 0; i < Vec[CurX].size(); i++) {
-			int nextX = Vec[CurX][i].first;
-			int nextCost = Vec[CurX][i].second;
-			if (!visited[nextX]) {
-				visited[nextX] = true;
-				Q.push(make_pair(nextX, CurCost + nextCost));
+		for (int i = 0; i < v[s].size(); i++) {
+			if (visit[v[s][i].first] == false) {
+				visit[v[s][i].first] = true;
+				q.push({ v[s][i].first,distance + v[s][i].second });
 			}
 		}
-	};
+
+	}
 }
 
 int main() {
-	FIRST
-	cin >> N >> M;
-	for (int i = 0; i < (N - 1); i++) {
-		int X, Y, D;
-		cin >> X >> Y >> D;
-		Vec[X].push_back(make_pair(Y, D));
-		Vec[Y].push_back(make_pair(X, D));
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n >> m;
+
+	for (int i = 0; i < n - 1; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		v[a].push_back(make_pair(b, c));
+		v[b].push_back(make_pair(a, c));
+
 	}
-	for (int i = 0; i < M; i++) {
-		int X, Y;
-		cin >> X >> Y;
-		cout << BFS(X, Y) << "\n";
+
+	for (int i = 0; i < m; i++) {
+		memset(visit, false, sizeof(visit));
+		int a, b;
+		cin >> a >> b;
+		cout<<solve(a, b)<<"\n";
 	}
+
+
 
 	return 0;
 }
