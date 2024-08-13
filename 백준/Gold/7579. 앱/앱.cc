44bdc89fbadaa5831e2a767b2538a9ca1556214f
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-08-13 16:18:35 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,44 +1,50 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int N, M, ans, sum;
-int bite[101], cost[101];
+int n = 0;
+int m = 0;
+
+
+int cost[101];
+int bite[101];
 int dp[101][10001];
 
-int main()
-{
-	cin >> N >> M;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int sum_cost = 0;
+	cin >> n >> m;
 
-	for (int i = 1; i <= N; i++) cin >> bite[i];
-	for (int i = 1; i <= N; i++) 
-	{
-		cin >> cost[i];
-		// sum : 모든 비용들의 합
-		sum += cost[i];
+	//최대 비용은 비용은 정해져있음 이 비용중 가장 먼저 m에 과 똑같은 bite를 가진 비용울 출력하면됨
+
+	for (int i = 1; i <= n; i++) {
+		cin >> bite[i];
 	}
 
-	// dp[i][j] == i번째 앱까지 탐색했을 때 
-	// j비용을 소모해서 얻을 수 있는 최대 메모리
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 0; j <= sum; j++)
-		{
-			if (j - cost[i] >= 0)
+	for (int i = 1; i <= n; i++) {
+		cin >> cost[i];
+		sum_cost += cost[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= sum_cost; j++) {
+			if (j - cost[i] >= 0) {
 				dp[i][j] = max(dp[i][j], dp[i - 1][j - cost[i]] + bite[i]);
-			
+			}
 			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
 		}
 	}
 
-	for (int i = 0; i <= sum; i++)
-	{
-		if (dp[N][i] >= M)
-		{
-			cout << i << endl;
+	for (int i = 0; i <= sum_cost; i++) {
+		if (dp[n][i] >= m) {
+			cout << i;
 			break;
-		}	
+		}
 	}
+	
 
-
+	return 0;
 }
