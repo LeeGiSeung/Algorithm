80ff6deab5cede7c38e1e2ce81705bc10027f6a9
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-08-29 01:22:20 +0900

    Update 낚시왕.cc

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,195 +1,225 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
- 
-#define endl "\n"
-#define MAX 100 + 1
+#include <iostream>
+#include <vector>
+
 using namespace std;
- 
-struct Shark_Info
-{
-    int R;
-    int C;
-    int Speed;
-    int Direct;
-    int Size;
-    int Num;
+
+// s는 속력, d는 이동 방향, z는 크기이다
+
+struct SHARK {
+	int Speed = 0;
+	int Direction = 0;
+	int size = 0;
 };
- 
-int R, C, M, Answer;
-vector<int> MAP[MAX][MAX];
-vector<Shark_Info> Shark;
- 
-int dx[] = { 0, -1, 1, 0, 0 };
-int dy[] = { 0, 0, 0, 1, -1 };
- 
-bool Standard(int A, int B)
-{
-    if (Shark[A].Size > Shark[B].Size) return true;
-    return false;
+
+int r, c, m;
+SHARK null_map[101][101];
+SHARK map[101][101];
+SHARK mapCopy[101][101];
+
+int result = 0;
+
+void copy_map(SHARK x[101][101], SHARK y[101][101]) {
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			y[i][j] = x[i][j];
+		}
+	}
 }
- 
-void Input()
-{
-    cin >> R >> C >> M;
-    for (int i = 0; i < M; i++)
-    {
-        int r, c, s, d, z;
-        cin >> r >> c >> s >> d >> z;
-        Shark.push_back({ r,c,s,d,z,i });
-        MAP[r][c].push_back(i);
-    }
+
+void Hunting(int y) {
+	//낚시장 깊이를 넘지 않을 때 까지 반복
+	int x = 1;
+	while (x <= r) {
+		//만약 그 칸에 상어가 있으면
+		if (map[x][y].size > 0) {
+			//죽이기 전에 무게 추가
+			result += map[x][y].size;
+			//상어 죽임
+			map[x][y].size = 0;
+			map[x][y].Direction = 0;
+			map[x][y].Speed = 0;
+			break;
+		}
+		else {
+			x++;
+		}
+	}
 }
- 
-bool Check()
-{
-    for (int i = 0; i < Shark.size(); i++)
-    {
-        if (Shark[i].Size != 0) return false;
-    }
-    return true;
+//d가 1인 경우는 위, 2인 경우는 아래, 3인 경우는 오른쪽, 4인 경우는 왼쪽
+//d가 1부터 시작이라서 그냥 5개로 만들었음
+int dx[5] = { 0,		-1,1,0,0 };
+int dy[5] = { 0,		0,0,1,-1 };
+
+void Swiming12(int i, int j, int d) {
+	int x = i;
+	int y = j;
+	int direction = d;
+	int speed = map[i][j].Speed;
+
+	//상어가 벽에 닿았는지 유무
+	int flag = 1;
+
+	//상어가 총 이동한 거리
+	int move = 0;
+
+	int nx = x;
+
+	while (move < speed) {
+
+		if (1 <= nx + dx[direction] && nx + dx[direction] <= r) {
+			nx = nx + dx[direction];
+			move++;
+		}
+		else {
+			//벽에 닿았을 경우 break;
+			flag = 0;
+		}
+		//벽에 닿았을 경우
+		if (flag == 0) {
+			if (direction == 1) {
+				direction = 2;
+			}
+			else {
+				direction = 1;
+			}
+			flag = 1;
+		}
+
+	}
+
+	//움직일려는 상어의 크기가 기존에 있던 상어의 크기보다 크거나, 이동할려는 자리에 상어가 없으면 이동 가능함
+	if (map[x][y].size > mapCopy[nx][y].size || mapCopy[nx][y].size == 0) {
+		mapCopy[nx][y] = map[x][y];
+		mapCopy[nx][y].Direction = direction;
+		//cout << "이동한 상어 위치" << nx << " " << y << endl;
+	}
+
+	// 만약 움직일 위치의 상어가 나보다 이미 크다면 현재 상어는 죽음 크기가 같은 상어는 없다고 문제에 나와있음
+	else {
+		map[x][y].size = 0;
+		map[x][y].Direction = 0;
+		map[x][y].Speed = 0;
+		//cout << "뒤진 위치" << nx << " " << y << endl;
+		//cout << "뒤짐" << endl;
+	}
+
 }
- 
-void Fishing(int Idx)
-{
-    for (int i = 1; i <= R; i++)
-    {
-        if (MAP[i][Idx].size() != 0)
-        {
-            Answer = Answer + Shark[MAP[i][Idx][0]].Size;
-            Shark[MAP[i][Idx][0]].Size = 0;
-            MAP[i][Idx].clear();
-            break;
-        }
-    }
+
+void Swiming34(int i, int j, int d) {
+	int x = i;
+	int y = j;
+	int direction = d;
+	int speed = map[i][j].Speed;
+
+	//상어가 벽에 닿았는지 유무
+	int flag = 1;
+
+	//상어가 총 이동한 거리
+	int move = 0;
+
+	int ny = y;
+
+	while (move < speed) {
+
+		if (1 <= ny + dy[direction] && ny + dy[direction] <= c) {
+			ny = ny + dy[direction];
+			move++;
+		}
+		else {
+			//벽에 닿았을 경우 break;
+			flag = 0;
+		}
+		//벽에 닿았을 경우
+		if (flag == 0) {
+			if (direction == 3) {
+				direction = 4;
+			}
+			else {
+				direction = 3;
+			}
+			flag = 1;
+		}
+	}
+
+	//움직일려는 상어의 크기가 기존에 있던 상어의 크기보다 크거나, 이동할려는 자리에 상어가 없으면 이동 가능함
+	if (map[x][y].size > mapCopy[x][ny].size || mapCopy[x][ny].size == 0) {
+		mapCopy[x][ny] = map[x][y];
+		mapCopy[x][ny].Direction = direction;
+		//cout << "이동한 상어 위치" << x << " " << ny << endl;
+	}
+
+	// 만약 움직일 위치의 상어가 나보다 이미 크다면 현재 상어는 죽음 크기가 같은 상어는 없다고 문제에 나와있음
+	else {
+		map[x][y].size = 0;
+		map[x][y].Direction = 0;
+		map[x][y].Speed = 0;
+		//cout << "뒤진 위치" << x << " " << ny << endl;
+		//cout << "뒤짐" << endl;
+	}
 }
- 
-void Move_Shark()
-{
-    for (int i = 0; i < Shark.size(); i++)
-    {
-        if (Shark[i].Size == 0) continue;
-        int x = Shark[i].R;
-        int y = Shark[i].C;
-        MAP[x][y].clear();
-    }
- 
-    for (int i = 0; i < Shark.size(); i++)
-    {
-        if (Shark[i].Size == 0) continue;
-        int x = Shark[i].R;
-        int y = Shark[i].C;
-        int Direct = Shark[i].Direct;
-        int Speed = Shark[i].Speed;
- 
-        if (Direct == 1 || Direct == 2)
-        {
-            int Rotate = (R - 1) * 2;
-            if (Speed >= Rotate) Speed = Speed % Rotate;
- 
-            for (int j = 0; j < Speed; j++)
-            {
-                int nx = x + dx[Direct];
-                int ny = y + dy[Direct];
-                if (nx < 1)
-                {
-                    Direct = 2;
-                    nx = nx + 2;
-                }
-                if (nx > R)
-                {
-                    Direct = 1;
-                    nx = nx - 2;
-                }
-                x = nx;
-                y = ny;
-            }
-        }
-        else
-        {
-            int Rotate = (C - 1) * 2;
-            if (Speed >= Rotate) Speed = Speed % Rotate;
- 
-            for (int j = 0; j < Speed; j++)
-            {
-                int nx = x + dx[Direct];
-                int ny = y + dy[Direct];
-                if (ny < 1)
-                {
-                    Direct = 3;
-                    ny = ny + 2;
-                }
-                if (ny > C)
-                {
-                    Direct = 4;
-                    ny = ny - 2;
-                }
-                x = nx;
-                y = ny;
-            }
-        }
-        
-        Shark[i].R = x;
-        Shark[i].C = y;
-        Shark[i].Direct = Direct;
-        MAP[x][y].push_back(i);
-    }
+
+void Start(int i, int j) {
+	int size = map[i][j].size;
+	int direction = map[i][j].Direction;
+	int speed = map[i][j].Speed;
+
+	if (direction == 1 || direction == 2) {
+		Swiming12(i, j, direction);
+	}
+	else if (direction == 3 || direction == 4) {
+		Swiming34(i, j, direction);
+	}
 }
- 
-void Kill_Shark()
-{
-    for (int i = 1; i <= R; i++)
-    {
-        for (int j = 1; j <= C; j++)
-        {
-            if (MAP[i][j].size() > 1)
-            {
-                sort(MAP[i][j].begin(), MAP[i][j].end(), Standard);
-                int Live_Index = MAP[i][j][0];
-                for (int k = 1; k < MAP[i][j].size(); k++)
-                {
-                    Shark[MAP[i][j][k]].Size = 0;
-                    Shark[MAP[i][j][k]].R = -1;
-                    Shark[MAP[i][j][k]].C = -1;
-                }
-                MAP[i][j].clear();
-                MAP[i][j].push_back(Shark[Live_Index].Num);
-            }
-        }
-    }
+
+void Move() {
+
+	//낚시장 검사
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			//만약 상어가 있으면
+			if (map[i][j].size > 0) {
+				//cout << "원래 상어 위치" << i << " " << j << endl;
+				//이동 시작
+				Start(i, j);
+				//cout << endl;
+			}
+		}
+	}
+
 }
- 
-void Solution()
-{
-    if (M == 0)
-    {
-        cout << 0 << endl;
-        return;
-    }
-    for (int i = 1; i <= C; i++)
-    {
-        if (Check() == true) break;
-        Fishing(i);
-        Move_Shark();
-        Kill_Shark();
-    }
-    cout << Answer << endl;
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
-    return 0;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> r >> c >> m;
+
+	for (int i = 0; i < m; i++) {
+		int x, y, s, d, z;
+		//r, c, s, d, z
+		cin >> x >> y >> s >> d >> z;
+		map[x][y] = { s,d,z };
+	}
+
+	//낚시왕은 처음에 1번 열의 한 칸 왼쪽에 있다. 다음은 1초 동안 일어나는 일이며, 아래 적힌 순서대로 일어난다. 낚시왕은 가장 오른쪽 열의 오른쪽 칸에 이동하면 이동을 멈춘다.
+
+
+	for (int i = 1; i <= c; i++) {
+		copy_map(null_map, mapCopy);
+		//cout << "낚시꾼 위치" << i << endl;
+		//1. 낚시왕이 오른쪽으로 한 칸 이동한다.
+		//2. 낚시왕이 있는 열에 있는 상어 중에서 땅과 제일 가까운 상어를 잡는다. 상어를 잡으면 격자판에서 잡은 상어가 사라진다.
+		Hunting(i);
+
+		//3.상어가 이동한다.
+		Move();
+
+		//mapCopy를 map에 복사
+		copy_map(mapCopy, map);
+	}
+
+	cout << result;
+
+	return 0;
 }
