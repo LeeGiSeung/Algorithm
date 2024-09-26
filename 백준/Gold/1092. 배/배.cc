7051c08bea776a57fffc6c4b8589f2d7f269fccc
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-09-27 01:05:05 +0900

    [Gold V] Title: 배, Time: 712 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.cc
+++ 백준/Gold/1092. 배/배.cc
@@ -1,45 +1,55 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
- 
+
+int n, m;
+
+vector<int> box;
+vector<int> crain;
+
+int ti = 0;
+
 int main() {
-    int N, M;
-    vector<int> crane;
-    vector<int> box;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        int n;
-        cin >> n;
-        crane.push_back(n);
-    }
-    cin >> M;
-    for (int i = 0; i < M; i++) {
-        int m;
-        cin >> m;
-        box.push_back(m);
-    }
-    sort(crane.begin(), crane.end());
-    sort(box.begin(), box.end());
-    int cnt = 0;
-    // 불가능한 경우
-    if (crane.back() < box.back()) {
-        cout << -1;
-        return 0;
-    }
-    while (!box.empty()) {
-        cnt++;
-        // 크레인 가장 큰 무게 부터
-        for (int i = crane.size() - 1; i >= 0; i--) {
-            // 상자 가장 큰 무게 부터
-            for (int j = box.size() - 1; j >= 0; j--) {
-                // 옮길 수 있으면 삭제하고 다음 크레인으로
-                if (crane[i] >= box[j]) {
-                    box.erase(box.begin() + j);
-                    break;
-                }
-            }
-        }
-    }
-    cout << cnt;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		crain.push_back(a);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		box.push_back(a);
+	}
+
+	sort(crain.begin(), crain.end());
+	sort(box.begin(), box.end());
+
+	if (box.back() > crain.back()) {
+		cout << -1;
+		return 0;
+	}
+
+	while (!box.empty()) {
+		ti++;
+		for (int i = crain.size()-1; i >= 0; i--) {
+			for (int j = box.size() -1; j >= 0; j--) {
+				if (crain[i] >= box[j]) {
+					box.erase(box.begin() + j);
+					break;
+				}
+			}
+		}
+	}
+
+	cout << ti;
+
+	return 0;
 }
