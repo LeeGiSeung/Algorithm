ab0b9ca25a422828c1e5de26174c69a774146956
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2024-10-07 15:16:36 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <algorithm>
 using namespace std;
+
+string s;
+
+string list[8] = { "dz=","c=","c-", "d-", "lj", "nj", "s=", "z=" };
+int ct = 0;
+
 int main() {
-    vector<string> croatian = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-    int idx;
-    string str;
-    cin >> str;
-    for(int i = 0; i < croatian.size(); i++)
-    {
-        while(1){
-            idx = str.find(croatian[i]);
-            if(idx == string::npos)
-                break;
-            str.replace(idx,croatian[i].length(),"#");
-        }
-    }
-    cout << str.length();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int idx;
+
+	cin >> s;
+
+	for (int i = 0; i < 8; i++) {
+		while (true) {
+			idx = s.find(list[i]);
+			if (idx == -1) {
+				break;
+			}		
+			s.replace(idx, list[i].length(), "#");
+		}
+	}
+
+	cout << s.length();
+
+	return 0;
 }
