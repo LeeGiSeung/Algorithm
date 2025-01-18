d4397006ba209303b217f4163a54e5947dd37e66
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-01-18 17:37:05 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n;
+
+	while (n--) {
+		cout << "SciComLove" << "\n";
+	}
+
+
+	return 0;
+}
