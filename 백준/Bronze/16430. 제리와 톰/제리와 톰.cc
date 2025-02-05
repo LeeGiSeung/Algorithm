a0358a68ec1e950c97acfa911987c70ea8955209
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-02-05 15:29:52 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> a >> b;
+
+	cout << b - a << " " << b;
+
+	return 0;
+}
