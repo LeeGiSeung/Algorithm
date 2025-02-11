bfbb32e3939ba63c077447fb1a5155aa9f7ced38
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-02-11 16:03:37 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+	
+	cin >> a >> b >> c >> d;
+
+	cout << a * 56 + b * 24 + c * 14 + d * 6;
+
+
+	return 0;
+}
