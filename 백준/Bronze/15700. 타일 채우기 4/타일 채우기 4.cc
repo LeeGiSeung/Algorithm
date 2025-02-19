fb2fc60edfc141001a185c5c92ba628e4d2d49be
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-02-19 15:45:48 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+long long n, m;
+
+int main() {
+
+	cin >> n >> m;
+
+	long long k = n * m;
+
+	cout << k / 2;
+
+	return 0;
+}
