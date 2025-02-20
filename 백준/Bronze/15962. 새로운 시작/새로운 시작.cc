754da5d8dc9aeb5070ee97ea5848ac2dfcfc8847
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-02-20 15:39:45 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+	cout << "파이팅!!";
+
+	return 0;
+}
