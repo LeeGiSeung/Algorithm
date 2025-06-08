66a5d427b5f7c998547250f2fa7e5419e29ab5a6
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-06-08 15:36:49 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 3.92 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -2,69 +2,66 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int k,l,b;
 
-int k,l,limit;
-
-void turn(vector<vector<int>> &in){
-    int s = in.size();
-    vector<vector<int>> v(s, vector<int>(s));
-    for(int i = 0; i<s; i++){
-        for(int j = 0; j<s; j++){
-           v[i][j] = in[s - j - 1][i];
+void turn(vector<vector<int>> &key){
+    vector<vector<int>> v(k, vector<int>(k,0));
+    
+    for(int i = 0; i<k; i++){
+        for(int j = 0; j<k; j++){
+            v[i][j] = key[k - j - 1][i];
         }
     }
-    in = v;
+    
+    key = v;
+    
 }
 
 bool check(vector<vector<int>> &board, vector<vector<int>> &key, int x, int y){
-    bool check = true;
+    bool ch = true;
     
-    //x,y index 위치임
-    
-    for(int i = 0; i<key.size(); i++){
-        for(int j = 0; j<key.size(); j++){
-            board[i + x][j + y] += key[i][j]; 
+    for(int i = 0; i<k; i++){
+        for(int j = 0; j<k; j++){
+            board[i + x][j + y] += key[i][j];
         }
     }
     
     for(int i = 0; i<l; i++){
         for(int j = 0; j<l; j++){
             if(board[i + k - 1][j + k - 1] != 1){
-                check = false;
-            }; 
+                ch = false;
+            }
         }
     }
     
-    for(int i = 0; i<key.size(); i++){
-        for(int j = 0; j<key.size(); j++){
-            board[i + x][j + y] -= key[i][j]; //다시 빼줌
+    for(int i = 0; i<k; i++){
+        for(int j = 0; j<k; j++){
+            board[i + x][j + y] -= key[i][j];
         }
     }
-    
-    return check;
+    return ch;   
 }
 
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     bool answer = false;
+ 
     k = key.size();
     l = lock.size();
-    limit = 2*(k-1) + l;
-    // 열쇠로 자물쇠를 열수 있으면 true를, 열 수 없으면 false
-        
-    vector<vector<int>> board(limit, vector<int>(limit, 0));
+    b = (k-1) * 2 + l; //전체 보드 크기
     
-    //보드에 lock 배열
+    vector<vector<int>> board(b, vector<int>(b,0)); //board 설정
+    
+    //보드에 자물쇠 배치
     for(int i = 0; i<l; i++){
         for(int j = 0; j<l; j++){
-            board[i + k - 1][j + k - 1] = lock[i][j]; 
+            board[i + k - 1][j + k - 1] = lock[i][j];
         }
-    }
+    }  
     
-    for(int z = 0; z<4; z++){ //90도씩 4번 회전
-        //index는 board 전체 크기에서 key 사이즈를 뺀만큼까지 key를 놓을 수 있음
-        for(int i = 0; i<=limit-k; i++){
-            for(int j = 0; j<=limit-k; j++){
-                if(check(board, key, i,j)) return true; //만약 정답되면 그냥 true return
+    for(int t = 0; t<4; t++){
+        for(int i = 0; i<= b - k; i++){
+            for(int j = 0; j<=b - k; j++){
+                if(check(board, key, i,j)) return true;;
             }
         }
         turn(key);
