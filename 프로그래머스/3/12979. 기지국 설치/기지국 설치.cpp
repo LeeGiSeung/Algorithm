b922e0e50060afbe5aafdd6617a83a2c96ac1129
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-02 12:19:27 +0900

    [level 3] Title: 기지국 설치, Time: 0.08 ms, Memory: 3.97 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -6,30 +6,25 @@ using namespace std;
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
-
-    sort(stations.begin(), stations.end());
-
-    int index = 1;
-    int stations_index = 0;
-
-    while (index <= n) {
-        if (index >= stations[stations_index] - w && index <= stations[stations_index] + w) {
-            //범위안에 있으면 그냥 지나감
-            index = stations[stations_index] + w + 1;
-            stations_index++;
+    int location = 1;
+    int range = w * 2 +1;
+    
+    for(int i = 0; i<stations.size(); i++){
+        int left = stations[i] - w;
+        int right = stations[i] + w;
+        
+        if(left > location){
+            int gap = left - location;
+            answer += (gap + range - 1) / range;
         }
-        else {
-            index += w * 2 + 1;
-            answer++;
-        }
+        
+        location = right + 1;
     }
-
+    
+    if(location <= n){
+        int gap = n - location + 1;
+        answer += (gap + range - 1) / range;
+    }
+    
     return answer;
 }
-
-int main() {
-    vector<int> v({ 9 });
-
-    solution(16, v, 2);
-}
-
