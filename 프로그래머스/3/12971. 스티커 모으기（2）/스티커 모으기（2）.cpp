75a1b676579332d6065cabfc045fee3d4d005adf
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-04 18:22:18 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.55 ms, Memory: 6.84 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 int dp[100001];
-
 int solution(vector<int> sticker)
 {
     if(sticker.size() <= 1){
@@ -13,39 +11,26 @@ int solution(vector<int> sticker)
     else if(sticker.size() == 2){
         return max(sticker[0], sticker[1]);
     }
-    
     int answer =0;
+    
+    //1번째 스티커를 뜯는 경우
     dp[0] = sticker[0];
     dp[1] = sticker[0];
-    //dp로 해결
     
-    for(int i = 2; i < sticker.size() - 1; i++){ //처음 스티커를 땐 경우
-
-        if(dp[i - 1] < dp[i - 2] + sticker[i]){
-            dp[i] = dp[i - 2] + sticker[i];
-        }
-        else{
-            dp[i] = dp[i-1];
-        }
-        
+    for(int i = 2; i<sticker.size() - 1; i++){
+        dp[i] = max(dp[i - 2] + sticker[i], dp[i-1]);
         answer = max(dp[i], answer);
     }
-
-
+    answer = max(answer, sticker[sticker.size() - 2]);
+    //2번째 스티커를 뜯는 경우
     dp[0] = 0;
     dp[1] = sticker[1];
-    //dp로 해결
     
-    for(int i = 2; i<sticker.size(); i++){ //두번째 스티커를 땐 경우
-        if(dp[i - 1] < dp[i - 2] + sticker[i]){
-            dp[i] = dp[i - 2] + sticker[i];
-        }
-        else{
-            dp[i] = dp[i-1];
-        }
-        
+    for(int i = 2; i<sticker.size(); i++){
+        dp[i] = max(dp[i - 2] + sticker[i], dp[i-1]);
         answer = max(dp[i], answer);
     }
+    answer = max(answer, sticker[sticker.size() - 1]);
     
     return answer;
 }
