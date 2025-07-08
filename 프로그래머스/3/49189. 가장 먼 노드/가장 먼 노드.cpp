bb056d72f51699553369161086c1836f729d5ea3
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-08 13:26:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 8.34 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,52 +1,54 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <queue>
-
 using namespace std;
 
-vector<int> node[20002];
-int max_count;
+vector<int> v[20001];
+bool check[20001];
+int answer = 0;
+int fall = 1;
+
 
 int solution(int n, vector<vector<int>> edge) {
-    int answer = 0;
-    // 1번 노드에서 가장 멀리 떨어진 노드의 갯수
-
-    for (int i = 0; i < edge.size(); i++) {
-        node[edge[i][0]].push_back(edge[i][1]);
-        node[edge[i][1]].push_back(edge[i][0]);
+    
+    for(int i = 0; i<edge.size(); i++){
+        int a = edge[i][0];
+        int b = edge[i][1];
+        
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-
-    queue<int> q;
-    vector<int> check(n + 1, -1);
-    check[1] = 0;
-    q.push(1);
-
-    while (!q.empty()) {
-        int index = q.front();
+    check[1] = true;
+    //1번 노드에서 가장 멀리 떨어진 노드의 갯수
+    queue<pair<int,int>> q;
+    q.push({1,1});
+    
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
-
-        for (int i = 0; i < node[index].size(); i++) {
-            if (check[node[index][i]] == -1) { //아직 방문 안했을때
-                check[node[index][i]] = check[index] + 1;
-                q.push(node[index][i]);
-            }
+        
+        if(y == fall){
+            answer+=1;
+            //cout<<"증가"<<fall<<endl;
+        }
+        else if(y > fall){
+            answer = 1;
+            fall = y;
+            //cout<<x<<"갱신"<<fall<<endl;
+        }
+        
+        for(int i = 0; i<v[x].size(); i++){
+            int next_node = v[x][i];
+            
+            if(check[next_node]) continue;
+            //cout<<next_node<<endl;
+            check[next_node] = true;
+            q.push({next_node, y+1});
         }
     }
-
-    int max_count = *max_element(check.begin(), check.end());
-
-    for (int i = 1; i <= n; i++) {
-        if (max_count == check[i]) answer++;
-    }
-
+    
     return answer;
-}
-
-int main() {
-    vector<vector<int>> v1({{ 3, 6 }, { 4, 3 }, { 3, 2 }, { 1, 3 }, { 1, 2 }, { 2, 4 }, { 5, 2 }});
-
-    solution(6, v1);
-
-    return 0;
 }
