43321811cb040c23fe11baea4b60474994a6ba9c
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-09 12:01:41 +0900

    [level 3] Title: 징검다리 건너기, Time: 5.68 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -1,61 +1,53 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
-int limit;
-bool solve(int _count, vector<int> _stones) {
-
-    int minus = 0;
-
-    for (int i = 0; i < _stones.size(); i++) {
-        if (_stones[i] - _count <= 0) {
-            minus++; 
+bool check(vector<int> stones, int k, int mid){
+    
+    int ch = 0;
+    
+    for(int i = 0; i<stones.size(); i++){
+        if(stones[i] - mid <= 0){
+            ch++;
         }
-        else { //연속으로 건널 수 있으면 minus는 초기화
-            minus = 0;
+        else{
+            ch = 0;
         }
-
-        if (limit == minus) return false;
-
+        
+        if(ch >= k){
+            return false;
+        }
+        
     }
-
+    
     return true;
 }
 
 int solution(vector<int> stones, int k) {
     int answer = 0;
-    limit = k;
-
-    int start = 0;
-    int end = *max_element(stones.begin(), stones.end()); //주소반환
-    int mid = 0;
-    //건널 수 있는 최대 돌다리는 end개가 최대임
-
-    while (start <= end) {
-        mid = (end + start) / 2; //이분탐색으로 중간값부터 검사
-
-        if (solve(mid, stones)) {
-            //가능하다는건 하나 더 늘려서 시험해봐도됨
-            start = mid + 1;
+    
+    int left = 0;
+    int right = 0;
+    for(int i = 0; i<stones.size(); i++){
+        right = max(right, stones[i]);
+    }
+    
+    while(left <= right){
+        int mid = (left + right) / 2; //mid 값 구하기
+        
+        if(check(stones, k, mid)){
+            //들어왔다는건 징검다리 건널 수 있다는거
+            //사람 수를 더 늘려봐야함
+            left = mid + 1;
         }
-        else {
-            //안된다는건 줄여봐야함
-            end = mid - 1;
+        else{
+            right = mid - 1;
         }
     }
     
-    answer = start;
-
+    answer = left;
+    
     return answer;
-}
-
-int main() {
-
-    vector<int> v({ 2, 4, 5, 3, 2, 1, 4, 2, 5, 1 });
-
-    solution(v, 3);
-
-    return 0;
 }
