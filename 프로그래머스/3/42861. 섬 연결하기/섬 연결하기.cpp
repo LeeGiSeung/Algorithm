a351ad923c1b2d66e9ffc06092b805b62b305b96
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-10 15:08:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,47 +1,46 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
-
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int check[101];
+struct Node{
+    int cur;
+    int dist;
+    
+    bool operator<(const Node& other) const {
+        return dist > other.dist;   
+    }
+};
 
-int find_island(int island) {
-    if (check[island] == island) return island;
-    return check[island] = find_island(check[island]);
-}
+bool check[101];
 
+//다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다.
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-
-    sort(costs.begin(), costs.end(), [](const auto& a, const auto& b) {
-        return a[2] < b[2];
-        });
-
-    for (int i = 0; i < n; i++) {
-        check[i] = i;
-    }
-
-    for (int i = 0; i < costs.size(); i++) {
-        int start = find_island(costs[i][0]);
-        int end = find_island(costs[i][1]);
-        int cost = costs[i][2];
-
-        if (start != end) { //연결이 안돼있으면
-            check[end] = start;
-            answer += cost;
+    
+    priority_queue<Node> pq;
+    pq.push({0,0});
+    
+    while(!pq.empty()){
+        Node cur_node = pq.top();
+        pq.pop();
+        
+        if(check[cur_node.cur]) continue;
+        
+        check[cur_node.cur] = true;
+        answer += cur_node.dist;
+        //cout<<cur_node.cur<<" 확인"<<endl;
+        for(int i = 0; i<costs.size(); i++){
+            if(costs[i][0] == cur_node.cur){
+                pq.push({costs[i][1], costs[i][2]});
+            }
+            else if(costs[i][1] == cur_node.cur){
+                pq.push({costs[i][0], costs[i][2]});
+            }
         }
     }
-
+    
     return answer;
-}
-
-int main() {
-
-    vector<vector<int>> v({{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}} );
-
-    solution(4, v);
-
-    return 0;
 }
