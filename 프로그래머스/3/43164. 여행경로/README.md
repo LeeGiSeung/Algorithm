dc18f6bba8946bea20c69fd82454f297530b4cd6
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-11 14:36:03 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#qna) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.03 ms
+메모리: 4.2 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 17:30:19
+2025년 07월 11일 14:35:59
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,61 +1,49 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <queue>
+#include <iostream>
 #include <algorithm>
-
+#include <queue>
 using namespace std;
-vector<vector<string>> v;
+
+bool check[10001];
 vector<string> answer;
-bool check[10002];
-bool ismatch = false;
-
-void dfs(string start, int count) {
-    answer.push_back(start);
-
-
-
-    if (count == v.size()) {
-        ismatch = true;
+bool solve(const vector<string> &a, const vector<string> &b){
+    if(a[0] != b[0]) return a[0] < b[0];
+    else if(a[0] == b[0]){
+        return a[1] < b[1];
     }
+}
 
-    for (int i = 0; i < v.size(); i++) {
-        if (check[i]) continue;
-        if (v[i][0] == start) {
+void dfs(string s, vector<vector<string>> tickets, vector<string> c, bool &currect){
+    string cur = s;
+    
+    if(currect) return;
+    
+    c.push_back(s);
+    
+    if(tickets.size() + 1 == c.size()){
+        answer = c;
+        currect = true;
+    }
+    
+    for(int i = 0; i<tickets.size(); i++){
+        if(check[i]) continue;
+        if(s == tickets[i][0]){
             check[i] = true;
-            dfs(v[i][1], count + 1);
-            
-            if (!ismatch) {
-                check[i] = false;
-                answer.pop_back();
-            }
-
+            dfs(tickets[i][1],tickets, c, currect);
+            check[i] = false;
         }
     }
-
-
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
-
-
-    sort(tickets.begin(), tickets.end(), [](const auto &a, const auto&b) {
-        return a[1] < b[1];
-        });
-
-    v = tickets;
-
-    dfs("ICN", 0);
-
+    //항상 "ICN" 공항에서 출발
+    
+    sort(tickets.begin(), tickets.end(), solve);
+    vector<string> c;
+    bool currect = false;
+    
+    dfs("ICN",tickets,c, currect);
+    
     return answer;
-}
-
-int main() {
-
-    vector<vector<string>> v({{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}} );
-    //vector<vector<string>> v({ {"ICN", "ABC"}, {"ICN", "BBB"}, {"JFK", "HND"} });
-
-    solution(v);
-
-    return 0;
 }
