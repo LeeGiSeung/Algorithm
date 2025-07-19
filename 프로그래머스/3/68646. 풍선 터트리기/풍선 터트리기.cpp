665c2f152edee734509de6d2881d609aaa920927
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-19 18:10:22 +0900

    [level 3] Title: 풍선 터트리기, Time: 9.43 ms, Memory: 46.1 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -1,42 +1,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
-int left[1000001];
-int right[1000001];
+vector<int> left_v(1000001);
+vector<int> right_v(1000001);
 
 int solution(vector<int> a) {
     int answer = 0;
-    
-    //작은 풍선을 터트리는 행위는 딱 1번만 가능하다.
-    //원래는 큰 풍선을 터트리는게 맞음
-    int min = a[0];
-    
+    int leftmin = a[0];
     for(int i = 0; i<a.size(); i++){
-        if(a[i] < min){
-            min = a[i];
-        }
-        left[i] = min;
+        if(a[i] < leftmin) leftmin = a[i];
+        left_v[i] = leftmin;
     }
     
-    min = a[a.size() - 1];
+    int rightmin = a[a.size() - 1];
     for(int i = a.size() - 1; i >= 0; i--){
-        if(a[i] < min){
-            min = a[i];
-        }
-        right[i] = min;
+        if(a[i] < rightmin) rightmin = a[i];
+        right_v[i] = rightmin;
     }
     
     for(int i = 0; i<a.size(); i++){
-        int count = 0;
-        if(a[i] > left[i]) count++;
-        if(a[i] > right[i]) count++;
-        
-        if(count < 2){
-            answer++;
-        }
+        if(left_v[i] >= a[i] || right_v[i] >= a[i]) answer++;
     }
     
     return answer;
