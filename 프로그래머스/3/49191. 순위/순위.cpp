e43ec704bfda257bcba73aade667bde4801c6bb5
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-20 18:44:57 +0900

    [level 3] Title: 순위, Time: 1.35 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,42 +1,36 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
-bool check[102][102];
-
+vector<vector<bool>> check(101, vector<bool>(101, false));
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
     
     for(int i = 0; i<results.size(); i++){
-        int x = results[i][0];
-        int y = results[i][1];
-        
-        check[x][y] = true;
-        //x가 y를 이겼다.    
+        check[results[i][0]][results[i][1]] = true;
     }
     
-    for(int i = 1; i<= n; i++){
+    for(int i = 1; i<=n; i++){
         for(int j = 1; j<=n; j++){
             for(int k = 1; k<=n; k++){
-                if(check[j][i] && check[i][k]){
-                    check[j][k] = true;
-                }
-            }    
+                if(check[j][i] && check[i][k]) check[j][k] = true;
+            }
         }
     }
     
     for(int i = 1; i<=n; i++){
         int count = 0;
         for(int j = 1; j<=n; j++){
-            if(check[i][j] || check[j][i]){
-                count++;
-            }
+            if(i == j) continue;
+            
+            if(check[i][j] || check[j][i]) count++;
         }
-        if(n-1 == count) answer++;
+        
+        if(count == n-1) answer++;
     }
-    
     
     return answer;
 }
