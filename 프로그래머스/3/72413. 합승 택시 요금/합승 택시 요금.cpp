41c3c587eaaccada574322848d0bf57e8a24dd22
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-07-29 14:35:32 +0900

    [level 3] Title: 합승 택시 요금, Time: 4.05 ms, Memory: 7.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,44 +1,49 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <queue>
 #include <iostream>
+#include <queue>
 using namespace std;
-
-int dist[202][202];
+vector<pair<int,int>> node[201];
+vector<int> solve(int cur, int n){
+    vector<int> v(n + 1, 1e8);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    v[cur] = 0;
+    pq.push({cur,0});
+    
+    while(!pq.empty()){
+        int cur = pq.top().first;
+        int cost = pq.top().second;
+        pq.pop();
+        
+        for(int i = 0; i<node[cur].size(); i++){
+            int next = node[cur][i].first;
+            int next_cost = node[cur][i].second + cost;
+            
+            if(v[next] < next_cost) continue;
+            v[next] = next_cost;
+            pq.push({next, next_cost});
+        }
+    }
+    
+    return v;
+}
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     int answer = 1e8;
     
-    for(int i = 1; i<= n; i++){
-        for(int j = 1; j<=n; j++){
-            if(i == j) continue;
-            dist[i][j] = 1e8;
-        }
+    for(vector<int> v : fares){
+        int x = v[0]; int y = v[1]; int z = v[2];
+        node[x].push_back({y,z});
+        node[y].push_back({x,z});
     }
     
-    for(int i = 0; i<fares.size(); i++){
-        int from = fares[i][0];
-        int to = fares[i][1];
-        int cost = fares[i][2];
-        dist[from][to] = cost;
-        dist[to][from] = cost;
-    }
-    
-    //중간노드
-    //시작
-    //끝
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
-            for(int z = 1; z<=n; z++){
-                if(j == z) continue;
-                dist[j][z] = min(dist[j][z], dist[j][i] + dist[i][z]);
-            }
-        }
-    }
+    vector<int> sv = solve(s,n);
+    vector<int> av = solve(a,n);
+    vector<int> bv = solve(b,n);
     
     for(int i = 1; i<=n; i++){
-        answer = min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
+        answer = min(answer, sv[i] + av[i] + bv[i]);
     }
     
     return answer;
