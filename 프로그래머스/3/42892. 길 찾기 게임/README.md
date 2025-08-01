ec6bae9c782cc7abb471d25d122fe698d2cf07e9
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-08-01 15:10:23 +0900

    [level 3] Title: 길 찾기 게임, Time: 22.96 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/README.md
+++ 프로그래머스/3/42892. 길 찾기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 길 찾기 게임 - 42892 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 34.23 ms
+메모리: 9.1 MB, 시간: 22.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 11일 15:48:34
+2025년 08월 01일 15:10:19
 
 ### 문제 설명
 
--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -3,78 +3,98 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
+int n;
 struct Node{
-    int x;
-    int y;
-    int idx;
     Node* left;
     Node* right;
-    Node(int _x, int _y, int _idx) : x(_x), y(_y), idx(_idx), left(nullptr), right(nullptr) {}
+    int num;
+    int x;
+    Node(Node* left, Node* right, int num, int x):left(left), right(right), num(num), x(x){}
 };
 
-void insert(Node* parent, Node* child){
-    if(child->x < parent->x){
-        //작으면 왼쪽임
-        if(parent->left == nullptr) parent->left = child; //비어있으면 그냥 가고
-        else insert(parent->left, child); //안비어있으면 이어서
-    }    
-    else
-    {
-        //작으면 왼쪽임
-        if(parent->right == nullptr) parent->right = child; //비어있으면 그냥 가고
-        else insert(parent->right, child); //안비어있으면 이어서
+bool solve(const vector<int> &a, const vector<int> &b){
+    if(a[1] != b[1]) return a[1] > b[1];
+    return a[0] < b[0];
+    
+}
+
+void find_node(Node* top, Node* cur){
+    if(cur->x < top->x){ //left 가야함
+        if(top->left == nullptr){
+            top->left = cur; //비어있으면 그냥 넣고
+        }
+        else{
+            //안비어있으면 더 들어감
+            find_node(top->left, cur);
+        }
+    }
+    if(cur->x > top->x){ //left 가야함
+        if(top->right == nullptr){
+            top->right = cur; //비어있으면 그냥 넣고
+        }
+        else{
+            //안비어있으면 더 들어감
+            find_node(top->right, cur);
+        }
     }
 }
 
-bool solve(const Node* a, const Node* b){
-    if(a->y == b->y) return a->x < b->x;
-    return a->y > b->y;
+void find_pre(Node* top, vector<int> &pre){
+    if(pre.size() == n) return;
+    
+    pre.push_back(top->num);
+    if(top->left != nullptr){
+        find_pre(top->left, pre);
+    }
+    
+    if(top->right != nullptr){
+        find_pre(top->right, pre);
+    }
 }
 
-void find_pre (Node* cur, vector<int> &res){
-    //전위
-    if(cur == nullptr) return;
-    res.push_back(cur->idx);
-    find_pre(cur->left, res);
-    find_pre(cur->right, res);
-}
-
-void find_pro (Node* cur, vector<int> &res){
-
-    if(cur == nullptr) return;
-    find_pro(cur->left, res);
-    find_pro(cur->right, res);
-    res.push_back(cur->idx);
+void find_pro(Node* top, vector<int> &pro){
+    if(pro.size() == n) return;
+    
+    if(top->left != nullptr){
+        find_pro(top->left, pro);
+    }
+    
+    if(top->right != nullptr){
+        find_pro(top->right, pro);
+    }
+    pro.push_back(top->num);
 }
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     vector<vector<int>> answer;
-    vector<Node*> nodes;
+    vector<Node*> nodelist;
+    n = nodeinfo.size();
+    //nodeinfo는 이진노드의 좌표 y값이 작을수록 상위 노드임
     
-    for(int i = 0; i<nodeinfo.size(); i++){
-        nodes.push_back(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
+    
+    for(int i = 0; i<n; i++){
+        nodeinfo[i].push_back(i+1);
     }
+    //정렬하기전에 num 넣어야함
+    sort(nodeinfo.begin(), nodeinfo.end(), solve);
     
-    sort(nodes.begin(), nodes.end(), solve);
+    Node* top = new Node(nullptr, nullptr, nodeinfo[0][2], nodeinfo[0][0]);
+    int index = 0;
     
-    for(int i = 0; i<nodes.size(); i++){
-        cout<<nodes[i]->x<<endl;
+    //for(int i = 0; i<nodeinfo.size(); i++){
+        //cout<<nodeinfo[i][0]<<" "<<nodeinfo[i][1]<<endl;
+    //}
+    
+    
+    for(int i = 1; i<nodeinfo.size(); i++){
+        Node* cur = new Node(nullptr, nullptr, nodeinfo[i][2], nodeinfo[i][0]);
+        find_node(top, cur);
     }
-    
-    Node* root = nodes[0]; //최상위 루트
-    
-    for(int i = 1; i<nodes.size(); i++){
-        insert(root, nodes[i]);
-    }
-    
     vector<int> pre;
     vector<int> pro;
-    find_pre(root, pre);
-    find_pro(root, pro);
-    
+    find_pre(top, pre);
+    find_pro(top, pro);
     answer.push_back(pre);
     answer.push_back(pro);
-    
     return answer;
 }
