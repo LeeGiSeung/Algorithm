7016351f1b71e16af05f9ecca0744ff8e0d97be7
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-08-06 15:11:05 +0900

    [level 3] Title: N으로 표현, Time: 11.48 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,57 +1,43 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 #include <set>
 using namespace std;
 
-int solve(int n, int c){
-    int rt = n;
-    
-    for(int i = 0; i<c; i++){
-        rt = (rt * 10) + n;
+int solution(int N, int number) {
+    int answer = -1;
+    set<int> s[8];
+    int num = 0;
+    for(int i = 0; i<8; i++){
+        num = num * 10 + N; //5 55 555
+        s[i].insert(num);
+        //cout<<num<<endl;
     }
     
-    return rt;
-}
-
-int solution(int N, int number) {
-    int answer = 0;
-    if(N == number) return 1;
-    
-    //n은 1~9사이
-    vector<set<int>> dp(9);
-    
-    dp[0].insert(N); //N 1개로 표현할 수 있는 수는 N 밖에 없음 ex) 5 1개로는 5
-    
-    for(int i = 1; i<8; i++){ //2개 ~ 8개 사용
-        // 55, 555, 5555
-        int mulint = solve(N, i);
-        dp[i].insert(mulint);
-        for(int x = 0; x<i; x++){
-            for(int y = 0; y <i; y++){
-                if(x + y + 1 != i) continue; //x개 사용과 y개 사용을 검사해야함
-                
-                //사칙 연산 집어넣기
-                for(int a : dp[x]){ //x개 쓴거랑 y개 쓴거랑 섞기
-                    for(int b : dp[y]){
-                        dp[i].insert(a + b);
-                        if(a - b > 0){
-                            dp[i].insert(a - b);
-                        }
-                        dp[i].insert(a * b);
-                        if(a / b > 0){
-                            dp[i].insert(a / b);
-                        }
-                    }
+    for(int i = 1; i<8; i++){
+        for(int j = 0; j<i; j++){
+            for(int a : s[j]){
+                 for(int b : s[i-j-1]){
+                    s[i].insert(a+b);
+                    s[i].insert(a-b);
+                    s[i].insert(a*b);
+                    if(b != 0)
+                        s[i].insert(a/b);
                 }
             }
         }
-        if(dp[i].find(number) != dp[i].end()){
-            answer = i + 1;
-            break;
-        }
     }
     
-    if(!answer) answer = -1;
+    for(int i = 0; i < 8; ++i){
+        if(s[i].find(number) != s[i].end()){
+            answer = i + 1;
+            break;
+        }  
+    }
+
+
+    //N과 사칙연산만 사용해서 표현 할 수 있는 방법 중 N 사용횟수의 최솟값을 return 하도록 
     
     return answer;
 }
