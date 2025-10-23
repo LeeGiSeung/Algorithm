7ea23cfc86bf5c138e8c3c8f933102a9ea4cf6a1
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-23 16:45:59 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,49 +1,38 @@
 #include <string>
 #include <vector>
-#include <string.h>
-#include <algorithm>
 #include <iostream>
 using namespace std;
-int check[101][101];
+
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
+    int pw = 1000000007;
+    int mincount;
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    //int dp[101][101];
     
-    //puddles 물에 잠긴 지역 0~10개
-    memset(check, 1e8, sizeof(check));
-    
-    for(int i = 0; i<puddles.size(); i++){
-        int x = puddles[i][0];
-        int y = puddles[i][1];
-        check[x][y] = -1; //연못으로 설정
+    for(vector<int> v : puddles){
+        int x = v[1];
+        int y = v[0];
+        
+        dp[x][y] = -1;
     }
     
-    for(int i = 0; i<=m; i++){
-        check[i][0] = 0;
-    }
+    dp[1][1] = 1;
     
-    for(int i = 0; i<=n; i++){
-        check[0][n] = 0;
-    }
-    
-    check[1][1] = 1;
-    
-    for(int i = 1; i<=m; i++){
-        for(int j = 1; j<=n; j++){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
             if(i == 1 && j == 1) continue;
-            if(check[i][j] == -1) continue; //연못이면 넘어감
-            if(check[i - 1][j] == -1){
-                check[i][j] = check[i][j-1] % 1000000007;
-            }
-            else if(check[i][j - 1] == -1){
-                check[i][j] = check[i - 1][j] % 1000000007;
-            }
-            else{
-                check[i][j] = (check[i][j - 1] + check[i - 1][j]) % 1000000007;
-            }
+            int a = 0;
+            int b = 0;
+            //i 혹은 j가 1이면 가장 가장자리임 가장자리는 경우의 수가 딱 1밖에없 이전거 그대로 들고옴
+            if(dp[i][j] == -1) continue; //물가면 그냥 넘김
+            if(dp[i - 1][j] != -1) a = dp[i-1][j];
+            if(dp[i][j - 1] != -1) b = dp[i][j-1];
+   
+            dp[i][j] = (a+b) % pw;
+            
         }
     }
     
-    answer = check[m][n];    
-    
-    return answer;
+    return dp[n][m] % pw;
 }
