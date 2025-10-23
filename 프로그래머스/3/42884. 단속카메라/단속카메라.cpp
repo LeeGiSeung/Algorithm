bb4a5be9457cedf0bb214bd55688b23c4b39f7c0
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-23 17:08:13 +0900

    [level 3] Title: 단속카메라, Time: 0.96 ms, Memory: 4.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,27 +1,27 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
+bool st(vector<int> &a, vector<int> &b)
+{
+    return a[1] < b[1];
+}
+
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
+    int answer = 1;
     
-    //최소 몇 대의 카메라를 설치해야 하는지를
-    sort(routes.begin(), routes.end());
+    sort(routes.begin(), routes.end(), st);
     
-    int camera = routes[0][1];
-    answer++;
-    
-    for(int i = 1; i<routes.size(); i++){
-        //카메라 들어오는 위치가 camera보다 크면
-        //카메라 새로 설치해야함
-        if(routes[i][0] > camera){ 
-            camera = routes[i][1]; //새로운 카메라의 제한은 현재 카메라가 끝나는 부분 
-            answer++; //카메라 증가
-        }
-        else if(routes[i][1] < camera){
-            camera = routes[i][1];
+    int end = routes[0][1];
+    cout<<end<<endl;
+    for(vector<int> v:routes){
+        int start = v[0]; //현재 카메라의 시작
+        if(start > end){ //현재 카메라가 이전 카메라보다 넘어가면
+            answer++; //카메라 늘리고 현재 카메라를 이번 카메라의 end로 만듬
+            end = v[1];
+            cout<<end<<endl;
         }
     }
     
