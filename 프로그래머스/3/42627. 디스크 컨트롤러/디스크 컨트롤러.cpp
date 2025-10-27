1d3a4945dc91e775ccf4ad47d8c76d9ccfa70cd8
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-27 16:37:54 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.16 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,32 +1,45 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-bool solve(const vector<int> &a, const vector<int> &b){
-    return a[1] <= b[1];
-}
+struct Data {
+    int start;
+    int dist;
+    bool operator<(const Data& a) const {
+        return dist > a.dist; // 소요 시간이 짧은 순으로
+    }
+};
 
 int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    int time = 0;
-    int jobs_size = jobs.size();
-
-    sort(jobs.begin(), jobs.end(), solve);
+    sort(jobs.begin(), jobs.end()); // 시작 시간 기준 정렬
+    priority_queue<Data> pq;
     
-    while(!jobs.empty()){
-        for(int i = 0; i<jobs.size(); i++){
-            if(time >= jobs[i][0]){
-                time += jobs[i][1];
-                answer += (time - jobs[i][0]);
-                cout<<time - jobs[i][0]<<endl;
-                jobs.erase(jobs.begin() + i);
-                break;
-            }
-            if(i == jobs.size() - 1) time++;
+    int time = 0;
+    int index = 0;
+    int total = 0;
+    int count = 0;
+    
+    while (count < jobs.size()) {
+        // 현재 시간에 들어온 작업들 추가
+        while (index < jobs.size() && jobs[index][0] <= time) {
+            pq.push({ jobs[index][0], jobs[index][1] });
+            index++;
+        }
+
+        if (!pq.empty()) {
+            Data cur = pq.top();
+            pq.pop();
+            time += cur.dist; // 작업 진행
+            total += time - cur.start; // 완료 시간 - 요청 시간
+            count++;
+        } else {
+            // 대기 중인 작업이 없으면 다음 작업이 들어올 때까지 시간 이동
+            time = jobs[index][0];
         }
     }
-    
-    return answer / jobs_size;
+
+    return total / jobs.size();
 }
