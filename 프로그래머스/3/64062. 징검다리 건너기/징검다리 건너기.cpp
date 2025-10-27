71026a7298a23b4a4afd06e41655405ab8031421
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-27 15:29:39 +0900

    [level 3] Title: 징검다리 건너기, Time: 5.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -2,52 +2,49 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <string.h>
 using namespace std;
-
-bool check(vector<int> stones, int k, int mid){
-    
-    int ch = 0;
-    
-    for(int i = 0; i<stones.size(); i++){
-        if(stones[i] - mid <= 0){
-            ch++;
-        }
-        else{
-            ch = 0;
-        }
-        
-        if(ch >= k){
-            return false;
-        }
-        
-    }
-    
-    return true;
-}
 
 int solution(vector<int> stones, int k) {
     int answer = 0;
     
+    //k = 한번에 건너뛸 수 있는 돌의 개수
+    
+    //건널 수 있는 사람의 최대수는 stones의 최대수임
+    
     int left = 0;
     int right = 0;
+    
     for(int i = 0; i<stones.size(); i++){
-        right = max(right, stones[i]);
+        right = max(stones[i], right);
     }
     
     while(left <= right){
-        int mid = (left + right) / 2; //mid 값 구하기
-        
-        if(check(stones, k, mid)){
-            //들어왔다는건 징검다리 건널 수 있다는거
-            //사람 수를 더 늘려봐야함
-            left = mid + 1;
+        int mid = (right + left) / 2;
+        //mid로 검사
+        int null = 0;
+        bool check = true;
+        //cout<<left<<" "<<mid<<" "<<right<<endl;
+        for(int i = 0; i<stones.size(); i++){
+            
+            if(stones[i] - mid < 0) null++;
+            else null = 0;
+                
+            if(null >= k){
+                check = false;
+                break;
+            }
         }
-        else{
+        
+        if(!check){
+            //징검다리 간격이 너무 큼 이러면 mid를 줄여야함
             right = mid - 1;
         }
+        else{
+            left = mid + 1; // mid를 좀 더 키워봄
+            answer = max(mid, answer);
+        }
     }
-    
-    answer = left;
     
     return answer;
 }
