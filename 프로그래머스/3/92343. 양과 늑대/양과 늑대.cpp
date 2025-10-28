e0290bd53136f0a5dca26db9b1730b5d82f4aa2b
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-28 23:34:22 +0900

    [level 3] Title: 양과 늑대, Time: 1.16 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -2,56 +2,41 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <string.h>
 using namespace std;
-
-vector<int> node[20]; //노드 연결 상태
-vector<int> visit[20];
-int check[20]; //노드가 양인지 늑대인지
-
-int max_sheep = 0;
-void solve(int cur, int sheep, int wolf, vector<int> visit){
-    if(check[cur] == 0) sheep++;
-    else if(check[cur] == 1) wolf++;
+int answer = 0;
+vector<vector<int>> node(18);
+vector<int> info;
+void dfs(int sheep, int wolf, vector<int> root){
+    answer = max(sheep, answer);
     
-    if(wolf >= sheep) return;
-    
-    max_sheep = max(max_sheep, sheep);
-    
-    vector<int> newvisit;
-    newvisit = visit;
-    newvisit.erase(remove(newvisit.begin(), newvisit.end(), cur), newvisit.end());
-    
-    for(int i = 0; i<node[cur].size(); i++){
-        newvisit.push_back(node[cur][i]);
+    for(int i = 0; i<root.size(); i++){ //root : 지금 현재 갈 수 있는 후보 루트들
+        int cur = root[i];
+        vector<int> copy = root;
+        
+        copy.erase(copy.begin() + i);
+        
+        for(int c : node[cur]) copy.push_back(c);
+        
+        if(info[cur] == 0){
+            dfs(sheep + 1, wolf, copy);
+        }
+        else{
+            if(sheep > wolf + 1){
+                dfs(sheep, wolf + 1, copy);
+            }
+        }
     }
-    
-    for(int i : newvisit){
-        solve(i, sheep, wolf, newvisit);
-    }
-    
-    return;
-    
 }
 
-int solution(vector<int> info, vector<vector<int>> edges) {
-    int answer = 0;
+int solution(vector<int> _info, vector<vector<int>> edges) {
     
-    check[0] = 0;
-    memset(visit, 0, sizeof(visit));
-    for(int i = 0; i<info.size(); i++){
-        check[i] = info[i];
+    info = _info;
+    
+    for(vector<int> v : edges){
+        node[v[0]].push_back(v[1]);
     }
     
-    for(int i = 0; i<edges.size(); i++){
-        int x = edges[i][0];
-        int y = edges[i][1];
-        
-        node[x].push_back(y);
-    }
-    vector<int> v;
-    v.push_back(0);
-    solve(0, 0, 0, v);
-    answer = max_sheep;
+    dfs(1,0,node[0]);
+    
     return answer;
 }
