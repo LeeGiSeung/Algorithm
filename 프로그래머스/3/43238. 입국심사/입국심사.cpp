174ec839266be6ecb33dfacb3e3cc1bb002dbed4
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-28 14:17:39 +0900

    [level 3] Title: 입국심사, Time: 39.05 ms, Memory: 6.77 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,36 +1,37 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
+#include <climits>
 using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-    long long left = 0;
-    int mx = 0;
-    for(int i = 0; i<times.size(); i++){
-        mx = max(mx, times[i]);
-    }
     
-    long long right = 1ll * mx * n;
-
+    long long left = 0;
+    long long right = 0;
+    
+    right = LLONG_MAX;
+    answer = right;
     while(left <= right){
         long long mid = (left + right) / 2;
+        //mid 시간으로 사람 몇 명을 볼 수 있는지
         long long count = 0;
         
-        for(int i = 0; i<times.size(); i++){
-            count += mid / times[i];
+        for(int i : times){
+            count += mid / i;
+            if(count >= n) break;
         }
         
         if(count >= n){
-            answer = left;
-            right = mid - 1;
+            answer = min(mid, answer);
+            right = mid - 1;//좀 더 극한으로 줄여봄
         }
-        else{
+        else if(count < n){
+            //부족하면 시간을 더 늘러야함
             left = mid + 1;
-        }
-    }
-    
-    answer = left;
+        }    
+    }    
     
     return answer;
 }
