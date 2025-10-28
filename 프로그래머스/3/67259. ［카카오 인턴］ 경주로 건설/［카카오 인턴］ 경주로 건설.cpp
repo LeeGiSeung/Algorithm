2a38811e8c6ceafe7f347d5d2018dec6d789ad2a
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-29 01:29:16 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.32 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -5,82 +5,66 @@
 #include <queue>
 #include <string.h>
 using namespace std;
-
 int check[26][26][2];
-
-struct DATA{
-    int x;
-    int y;
-    int cost;
-    int direction; //0 오른쪽 1 아래쪽
-};
-
-int dx[4] = {-1,1,0,0};
+int dx[4] = {-1,1,0,0}; //상, 하, 좌, 우
 int dy[4] = {0,0,-1,1};
 
-void bfs(int max_x, int max_y, vector<vector<int>> board){
-    queue<DATA> q;
-    q.push({0,0,0,0});
-    q.push({0,0,0,1});
-    check[0][0][0] = 0;
-    check[0][0][1] = 0;
-    while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
-        int cost = q.front().cost;
-        int direction = q.front().direction;
-        q.pop();
-        
-        if(x == max_x && y == max_y){
-            continue;
-        }
-        
-        for(int i = 0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            int ncost = cost;
-            //직진인지 커브인지
-            if(nx < 0 || ny < 0 || nx > max_x || ny > max_y) continue; //범위 넘으면 안됨
-            if(board[nx][ny] != 0) continue;
-            int ndir = (i<=1) ? 0 : 1;
-            if(ndir == direction){ 
-                //직진
-                ncost += 100;
-                if(check[nx][ny][ndir] > ncost || check[nx][ny][ndir] == -1){
-                    check[nx][ny][ndir] = ncost;
-                    //cout<<nx<<" "<<ny<<" "<<ndir<<" "<<ncost<<endl;
-                    q.push({nx,ny,ncost,direction});
-                }
-            }
-            else{ //방향이 다르다
-                ncost += 600;
-                if(check[nx][ny][ndir] > ncost || check[nx][ny][ndir] == -1){
-                    check[nx][ny][ndir] = ncost;
-                    //cout<<nx<<" "<<ny<<" "<<ndir<<" "<<ncost<<endl;
-                    q.push({nx,ny,ncost,ndir});
-                }
-            }
-        }
-    }
-}
+struct Data{
+    int x;
+    int y;
+    int dir;
+    int dist;
+};
 
 int solution(vector<vector<int>> board) {
     int answer = 0;
-    memset(check,-1,sizeof(check));
-    //직선 도로 하나를 만들 때는 100
-    //코너를 하나 만들 때는 500원이 추가로 듭니다.
-    int s = board.size() - 1;
-    bfs(s,s,board);
-
-    if(check[s][s][0] == -1){
-        answer = check[s][s][1];
+    int n = board.size();
+    queue<Data> q;
+    q.push({0,0,0,0});
+    q.push({0,0,1,0});
+    for(int i = 0; i < 26; i++)
+    for(int j = 0; j < 26; j++)
+        for(int k = 0; k < 2; k++)
+            check[i][j][k] = 1e8;
+    while(!q.empty()){
+        int x = q.front().x;
+        int y = q.front().y;
+        int dir = q.front().dir;
+        int dist = q.front().dist;
+        q.pop();
+        //0 1 수직, 수평
+        
+        if(x == n - 1 && y == n - 1){
+            check[x][y][dir] = min(check[x][y][dir], dist);
+            continue;
+        }
+        
+        //0 1 상하
+        //2 3 좌우
+        for(int i = 0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int ndist = dist + 100; //일단 기본적으로 100은 추가됨
+            int ndir = 0;
+            
+            if(dir == 0){ // 현재 수직
+                if(i == 2 || i == 3) ndist += 500; // 수평 이동이면 코너
+            } else { // 현재 수평
+                if(i == 0 || i == 1) ndist += 500; // 수직 이동이면 코너
+            }
+                
+            ndir = (i < 2 ? 0 : 1);
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue; //범위 넘으면 안됨
+            if(check[nx][ny][ndir] <= ndist) continue;
+            if(board[nx][ny] == 1) continue;
+            
+            check[nx][ny][ndir] = ndist;
+            q.push({nx,ny,ndir,ndist});
+        }
     }
-    else if(check[s][s][1] == -1){
-        answer = check[s][s][0];
-    }
-    else{
-        answer = min(check[s][s][0],check[s][s][1]);
-    }
+    
+    answer = min(check[n - 1][n - 1][0], check[n - 1][n - 1][1]);
     
     return answer;
 }
