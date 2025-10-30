00f8f885f7531a89ea8aedd065c7293b6d669e01
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2025-10-30 15:18:48 +0900

    [level 3] Title: 불량 사용자, Time: 46.06 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,58 +1,51 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 #include <set>
 using namespace std;
-set<string> s;
-vector<string> user;
-vector<string> ban;
-bool check[10];
 
-int check_ban(string user, string ban){
-    if(user.size() != ban.size()){
-        return 0;
+bool match(string user, string ban){
+    if(user.size() != ban.size()) return false;
+    
+    for(int i = 0; i<user.size(); i++){
+        if(ban[i] == '*') continue;
+        if(user[i] != ban[i]) return false;
     }
     
-    for(int i = 0; i<ban.size(); i++){
-        
-        if(ban[i] == '*') continue;
-        if(user[i] != ban[i]) return 0;
-        
-    }
-    return 1;
+    return true;
 }
 
-void dfs(int idx){
-    if(idx == ban.size()){
-        string str = "";
-        
-        for(int i = 0; i<user.size(); i++){
-            if(check[i]) str += i + '0';
-        }
-        
-        s.insert(str);
-        
+set<set<string>> answers;
+void dfs(int idx, vector<bool> &check, set<string> &s, vector<string> user_id, vector<string> banned_id){
+    if(idx == banned_id.size()){
+        answers.insert(s);
+        return;
     }
-    else
-    {
-        for(int i = 0; i<user.size(); i++){
-            if(check[i]) continue;
-
-            if(check_ban(user[i], ban[idx])){
-                check[i] = true;
-                dfs(idx + 1);
-                check[i] = false;
-            }
+    
+    for(int i = 0; i<user_id.size(); i++){
+        if(check[i]) continue;
+        if(match(user_id[i], banned_id[idx])){
+            check[i] = true;
+            s.insert(user_id[i]);
+            
+            dfs(idx + 1, check, s, user_id, banned_id);
+            
+            check[i] = false;
+            s.erase(user_id[i]);
         }
     }
-
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-    user = user_id;
-    ban = banned_id;
-    dfs(0);
+    int answer = 0;
     
-    return s.size();
+    vector<bool> check(user_id.size(), false);
+    set<string> s;
+    
+    int idx = 0;
+    
+    dfs(idx, check, s, user_id, banned_id);
+    answer = answers.size();
+    return answer;
 }
