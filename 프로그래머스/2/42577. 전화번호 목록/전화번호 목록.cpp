12b4ce8e548b87dd2559d40073ddf92fb1303920
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2026-05-15 12:34:20 +0900

    [level 2] Title: 전화번호 목록, Time: 56.87 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,27 +1,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
-
+#include <iostream>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-
+    
     sort(phone_book.begin(), phone_book.end());
-
-    for (int i = 0; i < phone_book.size() - 1; i++) {
-        if (phone_book[i + 1].find(phone_book[i]) == 0) { //찾았을경우 0번 인덱스부터 찾아야함
-            return false;
+    
+    for(int i = 0; i<phone_book.size() - 1; i++){
+        if(phone_book[i].size() > phone_book[i + 1].size()) continue; //앞놈 크기가 더 크면 비교안됨
+        bool check = false;
+        for(int j = 0; j<min(phone_book[i].size(), phone_book[i + 1].size()); j++){
+            if(phone_book[i][j] != phone_book[i + 1][j]){
+                check = true;
+                continue;
+            }
         }
+        
+        
+        if(!check) return false;
+    
     }
-
-    return true;
-}
-
-int main() {
-
-    vector<string> v({ "1124", "113", "112" });
-
-    solution(v);
+    
+    return answer;
 }
