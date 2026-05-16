24e83cb1940b9410389b48a994060f4f3e685871
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2026-05-16 22:48:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 7.53 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,37 +1,37 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <iostream>
 #include <set>
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    vector<int> answer(2);
-    multiset<int> st;
-    for(string s : operations){
-        int a = stoi(s.substr(s.find(" ") + 1));
+    vector<int> answer(2,0);
+    multiset<int> set;
+    
+    for(int i = 0; i<operations.size(); i++){
+        string s = operations[i];
+        
         if(s[0] == 'I'){
-            st.insert(a);
+            int num = stoi(s.substr(2, s.size()));
+            set.insert(num);
         }
-        else if(!st.empty()){
-            if(s == "D 1"){
-            //최대값 빼기
-            st.erase(--st.end());
+        else if(s[0] == 'D'){
+            if(set.size() == 0) continue;
+            if(s[2] == '-'){
+                set.erase(set.begin());
             }
-            else if(s == "D -1"){
-                //최소값 빼기
-                st.erase(st.begin());
+            else{
+                set.erase(prev(set.end()));
             }
         }
     }
     
-    if(st.empty()){
-        answer[0] = 0; answer[1] = 0;
+    if(set.size() == 0){
+        answer[0] = 0;
+        answer[1] = 0;
     }
     else{
-        answer[0] = *st.rbegin();
-        answer[1] = *st.begin();
+        answer[0] = *prev(set.end());
+        answer[1] = *set.begin();
     }
     
     return answer;
