a0cfb815971c8e9957c412c07b41bb02fc102e22
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2026-05-23 00:39:51 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 23.02 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,21 +1,32 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
     
-    //1 numbers를 2진수로 변환
-    //재귀를 통해 
     for(int i = 0; i<numbers.size(); i++){
-        long long cur = numbers[i];
-        if(cur % 2 == 0) answer.push_back(cur + 1);
+        if(numbers[i] % 2 == 0){
+            answer.push_back(numbers[i] + 1);
+        }
         else{
-            long long findone = (cur + 1) & (~cur);
-            long long ans = ~(findone >> 1) & (cur | findone);
-            answer.push_back(ans);
+            long long check = 1;
+            
+            while((numbers[i] & check) != 0){
+                check <<=1;
+            }
+
+            numbers[i] |= check;
+            
+            //1로킨거의 바로 뒤에있는걸 0으로 바꿈
+            //0111
+            //1000
+            
+            numbers[i] ^= (check>>1);
+            
+            answer.push_back(numbers[i]);
+            
         }
     }
     
