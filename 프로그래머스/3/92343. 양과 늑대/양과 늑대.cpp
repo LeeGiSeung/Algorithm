f59c4f15b62dc32a20ce03be45f7137c5b879b2a
이기승 <103581655+LeeGiSeung@users.noreply.github.com>
2026-05-27 01:43:52 +0900

    [level 3] Title: 양과 늑대, Time: 0.06 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,42 +1,73 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <queue>
+
 using namespace std;
-int answer = 0;
-vector<vector<int>> node(18);
-vector<int> info;
-void dfs(int sheep, int wolf, vector<int> root){
-    answer = max(sheep, answer);
+
+struct Data {
+    int Check;  
+    int Sheep;  
+    int Wolf;   
+};
+
+vector<int> node[18];
+bool visited[1 << 17]; 
+
+int solution(vector<int> info, vector<vector<int>> edges) {
+    int n = info.size();
+
+    for(const auto& edge : edges) {
+        node[edge[0]].push_back(edge[1]);
+    }
     
-    for(int i = 0; i<root.size(); i++){ //root : 지금 현재 갈 수 있는 후보 루트들
-        int cur = root[i];
-        vector<int> copy = root;
+    for(int i = 0; i < (1 << n); i++) visited[i] = false;
+    
+    queue<Data> q;
+    
+    int startCheck = 1; 
+    q.push({startCheck, 1, 0});
+    visited[startCheck] = true;
+    
+    int maxSheep = 1;
+    
+    while(!q.empty()) {
+        Data d = q.front();
+        q.pop();
         
-        copy.erase(copy.begin() + i);
+        int Check = d.Check;
+        int Sheep = d.Sheep;
+        int Wolf = d.Wolf;
         
-        for(int c : node[cur]) copy.push_back(c);
+        maxSheep = max(maxSheep, Sheep);
         
-        if(info[cur] == 0){
-            dfs(sheep + 1, wolf, copy);
-        }
-        else{
-            if(sheep > wolf + 1){
-                dfs(sheep, wolf + 1, copy);
+        for(int parent = 0; parent < n; parent++){
+            if(Check & (1<<parent)){ //지금 Check에서 접근이 되는 애면 접근함
+                for(int i = 0; i<node[parent].size(); i++){
+                    int nextNode = node[parent][i];
+                    
+                    //nextNode 간다고했을때 이게 이미 간건지 확인
+                    int nextCheck = Check | (1<<nextNode);
+                    
+                    if(visited[nextCheck]) continue; //이미 방문한거임
+                    
+                    int nextAnimal = info[nextNode];
+                    
+                    if(nextAnimal == 0){
+                        visited[nextCheck] = true;
+                        q.push({nextCheck, Sheep + 1, Wolf});
+                    }
+                    else{
+                        if(Wolf + 1 < Sheep){
+                            visited[nextCheck] = true;
+                            q.push({nextCheck, Sheep, Wolf + 1});
+                        }
+                    }
+                    
+                }
             }
         }
     }
-}
-
-int solution(vector<int> _info, vector<vector<int>> edges) {
     
-    info = _info;
-    
-    for(vector<int> v : edges){
-        node[v[0]].push_back(v[1]);
-    }
-    
-    dfs(1,0,node[0]);
-    
-    return answer;
+    return maxSheep;
 }
